8e2fda00bf801cc1a89ac3ad18651de6c57137aa
zoloypzuo(左亦平) <zoloypzuo@tencent.com>
2019-07-23 14:12:10 +0800

    Update wordcount.lua

--- src/ch11/wordcount.lua
+++ src/ch11/wordcount.lua
@@ -4,7 +4,9 @@
 
 local counter = {}
 
+-- open file and read lines
 for line in io.lines("alice_in_wonderland.txt") do
+    -- for each word in the line, increment its counter
     for word in string.gmatch(line, "%w+") do
         counter[word] = (counter[word] or 0) + 1
     end
@@ -12,6 +14,7 @@ end
 
 local words = {}
 
+-- NOTE 这里lua和python是不一样的，虽然pairs返回kv对，但是w对应k，而不是kv对
 for w in pairs(counter) do
     words[#words + 1] = w
 end
@@ -19,12 +22,15 @@ end
 table.sort(
     words,
     function(w1, w2)
-        return counter[w1] > counter[w2] or counter[w1] == counter[w2] and w1 < w2
+        local item1 = assert(counter[w1])
+        local item2 = assert(counter[w2])
+        return item1 > item2 or item1 == item2 and w1 < w2
     end
 )
 
-local n = math.min(tonumber(arg[1]) or math.huge,#words)
+-- NOTE tonumber(nil) returns nil
+local n = math.min(tonumber(arg[1]) or math.huge, #words)
 
-for i =1,n do
-    io.write(words[i],counter[words[i]],"\n")
+for i = 1, n do
+    io.write(words[i], counter[words[i]], "\n")
 end
