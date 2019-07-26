dfcb2e4b1f721cd28162d48a7bab7ade70335d07
zoloypzuo(左亦平) <zoloypzuo@tencent.com>
2019-07-26 15:15:43 +0800

    Update 1_values.lua

--- src/ch18_iterator/1_values.lua
+++ src/ch18_iterator/1_values.lua
@@ -1,3 +1,4 @@
+-- 7.1
 function values(t)
     local i = 0
     return function()
@@ -7,7 +8,14 @@ function values(t)
 end
 
 t = { 1, 2, 3 }
+--[[
 iter = values(t)
+while true do
+    local element = iter()
+    if element==nil then break end
+    print(element)
+end
+--]]
 
 for element in values(t) do
     print(element)
