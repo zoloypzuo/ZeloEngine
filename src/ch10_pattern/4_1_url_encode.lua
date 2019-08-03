a0da7b352af03c011a8a1015c059c1e00cf0401d
zoloypzuo <910181598@qq.com>
2019-08-03 13:02:22 +0800

    Update 4_1_url_encode.lua

--- src/ch10_pattern/4_1_url_encode.lua
+++ src/ch10_pattern/4_1_url_encode.lua
@@ -5,17 +5,48 @@
 -- * " " -> "+"
 -- * "(name=value&)*name=value"
 
-url_args = { name = "al"; query = "a+b = c"; q = "yes or no" }
-
-escaped_s = "name=al&query=a%2Bb+%3D+c&q=yes+or+no"
---assert(url_encode(url_args) == escaped_s)
-
 function unescape(s)
     s = string.gsub(s, "+", " ")
     s = string.gsub(s, "%%(%x%x)", function(h)
+        -- 将十六进制字符转为数字，再转为字符
         return string.char(tonumber(h, 16))
     end)
     return s
 end
 
-print(unescape(escaped_s))
+assert(unescape("a%2Bb+%3D+c") == "a+b = c")
+
+cgi = {}
+function decode(s)
+    for name, value in string.gmatch(s, "([^&=]+)=([^&=]+)") do
+        name = unescape(name)
+        value = unescape(value)
+        cgi[name] = value
+    end
+end
+
+function escape(s)
+    s = string.gsub(s, "[&=+%%%c]", function(c)
+        --print(c:byte())
+        return string.format("%%%02X", string.byte(c))
+    end)
+    s = string.gsub(s, " ", "+")
+    return s
+end
+
+function encode(t)
+    local b = {}
+    for k, v in pairs(t) do
+        print(k, v)
+        k = escape(k)
+        v = escape(v)
+        b[#b + 1] = (k .. "=" .. v)
+    end
+    return table.concat(b, "&")
+end
+
+args = { name = "al"; query = "a+b = c"; q = "yes or no" }
+
+url = "name=al&query=a%2Bb+%3D+c&q=yes+or+no"
+-- this will fail because table is iterated in a random order
+--assert(encode(args) == t)
