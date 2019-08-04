06e3857d113d49c9a739ffbb8742fc46a3f27434
zoloypzuo(左亦平) <zoloypzuo@tencent.com>
2019-08-05 07:27:49 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,46 @@
+// E:\ZeloEngine>cl
+// 用于 x86 的 Microsoft (R) C/C++ 优化编译器 19.21.27702.2 版
+// 版权所有(C) Microsoft Corporation。保留所有权利。
+
+// 用法: cl [ 选项... ] 文件名... [ /link 链接选项... ]
+
+// E:\ZeloEngine>cl main.c
+// 用于 x86 的 Microsoft (R) C/C++ 优化编译器 19.21.27702.2 版
+// 版权所有(C) Microsoft Corporation。保留所有权利。
+
+// main.c
+// Microsoft (R) Incremental Linker Version 14.21.27702.2
+// Copyright (C) Microsoft Corporation.  All rights reserved.
+
+// /out:main.exe
+// main.obj
+
+// E:\ZeloEngine>dir
+//  驱动器 E 中的卷是 Resources
+//  卷的序列号是 D490-8DF2
+
+//  E:\ZeloEngine 的目录
+
+// 2019/08/04  22:06    <DIR>          .
+// 2019/08/04  22:06    <DIR>          ..
+// 2019/08/04  22:03                66 .gitattributes
+// 2019/08/04  22:03             5,971 .gitignore
+// 2019/08/04  22:03             1,077 LICENSE
+// 2019/08/04  22:05                79 main.c
+// 2019/08/04  22:06           101,376 main.exe
+// 2019/08/04  22:06             1,378 main.obj
+// 2019/08/04  22:03                15 README.md
+//                7 个文件        109,962 字节
+//                2 个目录 166,228,471,808 可用字节
+
+// E:\ZeloEngine>main.exe
+// hello world
+
+
+
+#include <stdio.h>
+
+int main(){
+    printf("hello world");
+    return 0;
+}
