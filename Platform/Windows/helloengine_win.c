e4495debaa3672f6b7cce767ef3f600b18f7277a
zoloypzuo(左亦平) <zoloypzuo@tencent.com>
2019-08-05 16:29:27 +0800

    end update

---
+++ Platform/Windows/helloengine_win.c
@@ -0,0 +1,69 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd,
+                            UINT message,
+                            WPARAM wParam,
+                            LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPTSTR lpCmdLine,
+                   int nCmdShow)
+{
+    HWND hWnd;
+    WNDCLASSEX wc;
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpszClassName = _T("WindowClass1");
+
+    RegisterClassEx(&wc);
+
+    hWnd = CreateWindowEx(0,
+                          _T("WindowClass1"),
+                          _T("Hello, Engine!"),
+                          WS_OVERLAPPEDWINDOW,
+                          300,
+                          300,
+                          500,
+                          400,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+    default:
+        break;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
