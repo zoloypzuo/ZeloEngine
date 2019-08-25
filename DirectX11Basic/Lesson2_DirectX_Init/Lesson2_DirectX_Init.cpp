1729f4aee2cbe52e2f17e2982503b92d2acd784f
zoloypzuo <zoloypzuo@tencent.com>
2019-08-25 19:08:40 +0800

    unfinished refactor to D3DApp class, fix #3, fix #4

--- DirectX11Basic/Lesson2_DirectX_Init/Lesson2_DirectX_Init.cpp
+++ DirectX11Basic/Lesson2_DirectX_Init/Lesson2_DirectX_Init.cpp
@@ -36,142 +36,286 @@
 
 // just for convenice, BAD practice
 using namespace DirectX;
-using namespace DirectX::PackedVector;
+using namespace PackedVector;
 
+HRESULT hr{}; // used by V to check if a directx function succeeded
 
-HRESULT hr{};  // used by V to check if a directx function succeeded
+class D3DApp;
+D3DApp* g_pApp{};
 
-IDXGISwapChain* g_pSwapchain{};
-
-ID3D11Device* g_pDev{};
-
-ID3D11DeviceContext* g_pDevcon{};
-
-ID3D11RenderTargetView* g_pRTV{};
-
-void Initialize(HWND hWnd)
+class D3DApp
 {
+protected:
 	//
-	// create the swapchain
+	// some Direct3D objects
 	//
-	DXGI_SWAP_CHAIN_DESC scd{};
-	// fill the swap chain description struct
-	scd.BufferCount = 1;                                    // one back buffer
-	scd.BufferDesc.Width = 500;
-	scd.BufferDesc.Height = 400;
-	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
-	scd.BufferDesc.RefreshRate.Numerator = 60;
-	scd.BufferDesc.RefreshRate.Denominator = 1;
-	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
-	scd.OutputWindow = hWnd;                                // the window to be used
-	scd.SampleDesc.Count = 4;                               // how many multisamples
-	scd.Windowed = true;                                    // windowed/full-screen mode
-	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;     // allow full-screen switching
+	IDXGISwapChain* m_pSwapchain{};
+	ID3D11Device* m_pDevice{};
+	ID3D11DeviceContext* m_pDeviceContext{};
+	ID3D11RenderTargetView* m_pRtv{};
+	D3D11_VIEWPORT m_viewport{};
 
-	const D3D_FEATURE_LEVEL FeatureLevels[] = { D3D_FEATURE_LEVEL_11_1,
-											D3D_FEATURE_LEVEL_11_0,
-											D3D_FEATURE_LEVEL_10_1,
-											D3D_FEATURE_LEVEL_10_0,
-											D3D_FEATURE_LEVEL_9_3,
-											D3D_FEATURE_LEVEL_9_2,
-											D3D_FEATURE_LEVEL_9_1 };
-	D3D_FEATURE_LEVEL FeatureLevelSupported;
+	//
+	// some window configurations
+	//
+	std::wstring m_mainWndCaption{ L"D3D11 App" }; // TODO string or wstring?
+	int m_clientWidth{};
+	int m_clientHeight{};
 
-	// ReSharper disable once CppJoinDeclarationAndAssignment
-	V(D3D11CreateDeviceAndSwapChain(
-		nullptr, D3D_DRIVER_TYPE_HARDWARE,
-		nullptr, 0, FeatureLevels, _countof(FeatureLevels),
-		D3D11_SDK_VERSION, &scd, &g_pSwapchain, &g_pDev, &FeatureLevelSupported, &g_pDevcon));
+	/**
+	 * \brief the args in wWinMain
+	 */
+	struct
+	{
+		HINSTANCE hInstance;
+		HINSTANCE hPrevInstance;
+		LPWSTR lpCmdLine;
+		int nShowCmd;
+	} m_winMainArgs{};
 
-	// fallback, not used currently, because V will throw error if failed
-	if (hr == E_INVALIDARG) {
-		hr = D3D11CreateDeviceAndSwapChain(NULL,
-			D3D_DRIVER_TYPE_HARDWARE,
-			NULL,
-			0,
-			&FeatureLevelSupported,
-			1,
-			D3D11_SDK_VERSION,
-			&scd,
-			&g_pSwapchain,
-			&g_pDev,
-			NULL,
-			&g_pDevcon);
+	/**
+	 * \brief the window handle of the main window
+	 */
+	HWND m_hMainWnd{};
+
+	//
+	// some Direct3D configurations
+	//
+	D3D_DRIVER_TYPE m_driverType{ D3D_DRIVER_TYPE_HARDWARE };
+	bool m_enable4xMsaa{};
+	UINT m_4xMsaaQuality{};
+
+public:
+	/**
+	 * \brief save wWinMain args
+	 */
+	D3DApp(
+		HINSTANCE hInstance,
+		HINSTANCE hPrevInstance,
+		LPWSTR lpCmdLine,
+		int nShowCmd)
+	{
+		m_winMainArgs.hInstance = hInstance;
+		m_winMainArgs.hPrevInstance = hPrevInstance;
+		m_winMainArgs.lpCmdLine = lpCmdLine;
+		m_winMainArgs.nShowCmd = nShowCmd;
 	}
 
-	//
-	// create the rtv
-	//
-	ID3D11Texture2D* pBackBuffer;  // or ComPtr<ID3D11Texture3D>
-	// get the pointer to the backbuffer
-	V(g_pSwapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)& pBackBuffer));
-	// create a render target view from the backbuffer
-	V(g_pDev->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRTV));
-	SAFE_RELEASE(pBackBuffer);
-	// bind the view
-	g_pDevcon->OMSetRenderTargets(1, &g_pRTV, nullptr);
+	virtual ~D3DApp()
+	{
+		Finalize();
+	}
+
+	int Run()
+	{
+		// wait for the next msg in the queue
+		MSG msg{};
+		while (GetMessage(&msg, nullptr, 0, 0))
+		{
+			// translate msg into the right form
+			TranslateMessage(&msg);
+			// call WindowProc callback
+			DispatchMessage(&msg);
+		}
+
+		// return WM_QUIT ???
+		return msg.wParam;
+	}
+
+	int InitDirect3D()
+	{
+		HWND hWnd = m_hMainWnd;
+
+		//
+		// create the swapchain
+		//
+		DXGI_SWAP_CHAIN_DESC scd{};
+		// fill the swap chain description struct
+		scd.BufferCount = 1; // one back buffer
+		scd.BufferDesc.Width = 500;
+		scd.BufferDesc.Height = 400;
+		scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // use 32-bit color
+		scd.BufferDesc.RefreshRate.Numerator = 60;
+		scd.BufferDesc.RefreshRate.Denominator = 1;
+		scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // how swap chain is to be used
+		scd.OutputWindow = hWnd; // the window to be used
+		scd.SampleDesc.Count = 4; // how many multisamples
+		scd.Windowed = true; // windowed/full-screen mode
+		scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // allow full-screen switching
+
+		const D3D_FEATURE_LEVEL FeatureLevels[] = {
+			D3D_FEATURE_LEVEL_11_1,
+			D3D_FEATURE_LEVEL_11_0,
+			D3D_FEATURE_LEVEL_10_1,
+			D3D_FEATURE_LEVEL_10_0,
+			D3D_FEATURE_LEVEL_9_3,
+			D3D_FEATURE_LEVEL_9_2,
+			D3D_FEATURE_LEVEL_9_1
+		};
+		D3D_FEATURE_LEVEL FeatureLevelSupported;
+
+		// ReSharper disable once CppJoinDeclarationAndAssignment
+		V(D3D11CreateDeviceAndSwapChain(
+			nullptr, D3D_DRIVER_TYPE_HARDWARE,
+			nullptr, 0, FeatureLevels, _countof(FeatureLevels),
+			D3D11_SDK_VERSION, &scd, &m_pSwapchain, &m_pDevice, &FeatureLevelSupported, &m_pDeviceContext));
+
+		// fallback, not used currently, because V will throw error if failed
+		if (hr == E_INVALIDARG)
+		{
+			//hr = D3D11CreateDeviceAndSwapChain(NULL,
+			//	D3D_DRIVER_TYPE_HARDWARE,
+			//	NULL,
+			//	0,
+			//	&FeatureLevelSupported,
+			//	1,
+			//	D3D11_SDK_VERSION,
+			//	&scd,
+			//	&g_pSwapchain,
+			//	&g_pDev,
+			//	NULL,
+			//	&g_pDevcon);
+		}
+
+		//
+		// create the rtv
+		//
+		ID3D11Texture2D* pBackBuffer; // or ComPtr<ID3D11Texture3D>
+		// get the pointer to the backbuffer
+		V(m_pSwapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)& pBackBuffer));
+		// create a render target view from the backbuffer
+		V(m_pDevice->CreateRenderTargetView(pBackBuffer, nullptr, &m_pRtv));
+		SAFE_RELEASE(pBackBuffer);
+		// bind the view
+		m_pDeviceContext->OMSetRenderTargets(1, &m_pRtv, nullptr);
 
 
-	//
-	// set the viewport
-	//
-	D3D11_VIEWPORT viewport{};
-	viewport.TopLeftX = 0;
-	viewport.TopLeftY = 0;
-	viewport.Width = 500;  // TODO can be global const cfg
-	viewport.Height = 400;
-	g_pDevcon->RSSetViewports(1, &viewport);
+		//
+		// set the viewport
+		//
+		D3D11_VIEWPORT viewport{};
+		viewport.TopLeftX = 0;
+		viewport.TopLeftY = 0;
+		viewport.Width = 500; // TODO can be global const cfg
+		viewport.Height = 400;
+		m_pDeviceContext->RSSetViewports(1, &viewport);
 
-	// TODO init pipeline
+		// TODO init pipeline
 
-	// TODO init graphics
-}
-
-void Finalize()
-{
-	SAFE_RELEASE(g_pSwapchain);
-	SAFE_RELEASE(g_pDev);
-	SAFE_RELEASE(g_pDevcon);
-	SAFE_RELEASE(g_pRTV);
-}
-
-
-void RenderFrame()
-{
-	// clear the backbuffer
-	const FLOAT color[4] = { 0.0f, 0.2f, 0.4f, 1.0f };
-	g_pDevcon->ClearRenderTargetView(g_pRTV, color);
-
-	// do render here
-
-	// present the backbuffer
-	V(g_pSwapchain->Present(0, 0));
-}
-
-LRESULT CALLBACK WindowProc(HWND hWnd,
-	UINT message,
-	WPARAM wParam,
-	LPARAM lParam)
-{
-	switch (message) {
-	case WM_CREATE:
-		break;
-	case WM_DESTROY:  // when the window is closed
-		// close the app here
-		//DiscardGraphicResources();
-		PostQuitMessage(0);
+		// TODO init graphics
 		return 0;
-	case WM_SIZE:
-		break;
-	case WM_PAINT:
-		//CreateGraphicsResources(hWnd);
-		RenderFrame();
-	default:
-		break;
 	}
 
-	// default msg handle
-	V_RETURN(DefWindowProc(hWnd, message, wParam, lParam));
+	int Initialize()
+	{
+		if (InitMainWindow())
+		{
+			assert(false && "");
+			return -1;
+		}
+
+		if (InitDirect3D())
+		{
+			assert(false && "");
+			return -1;
+		}
+
+		return 0;
+	}
+
+	void Finalize()
+	{
+		SAFE_RELEASE(m_pSwapchain);
+		SAFE_RELEASE(m_pDevice);
+		SAFE_RELEASE(m_pDeviceContext);
+		SAFE_RELEASE(m_pRtv);
+	}
+
+
+	void RenderFrame()
+	{
+		// clear the backbuffer
+		const FLOAT color[4] = { 0.0f, 0.2f, 0.4f, 1.0f };
+		m_pDeviceContext->ClearRenderTargetView(m_pRtv, color);
+
+		// do render here
+
+		// present the backbuffer
+		V(m_pSwapchain->Present(0, 0));
+	}
+
+	static LRESULT CALLBACK WindowProc(HWND hWnd,
+		UINT message,
+		WPARAM wParam,
+		LPARAM lParam)
+	{
+		switch (message)
+		{
+		case WM_CREATE:
+			break;
+		case WM_DESTROY: // when the window is closed
+			// close the app here
+			//DiscardGraphicResources();
+			PostQuitMessage(0);
+			return 0;
+		case WM_SIZE:
+			break;
+		case WM_PAINT:
+			//CreateGraphicsResources(hWnd);
+			g_pApp->RenderFrame();
+		default:
+			break;
+		}
+
+		// default msg handle
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	};
+
+	int InitMainWindow()
+	{
+		// TODO a better implementation can be found at Introduction to 3D Game Programming with Direct3D
+		HINSTANCE hInstance = m_winMainArgs.hInstance;
+
+		// create the window class
+		WNDCLASSEX wc{};
+		wc.cbSize = sizeof(WNDCLASSEX);
+		wc.style = CS_HREDRAW | CS_VREDRAW;
+		wc.lpfnWndProc = WindowProc;
+		wc.hInstance = hInstance;
+		wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+		wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+		wc.lpszClassName = _T("WIndowClass1"); // NOTE that spell mistake, this name is used for create the window
+
+		if (!RegisterClassEx(&wc))
+		{
+			assert(false && "RegisterClassEx failed");
+			return -1;
+		}
+
+		HWND hWnd;
+		hWnd = CreateWindowEx(
+			0,
+			_T("WIndowClass1"),
+			_T("lpWindowName"),
+			WS_OVERLAPPEDWINDOW,
+			300, 300,
+			500, 400,
+			nullptr,
+			nullptr,
+			hInstance,
+			nullptr);
+		if (!hWnd)
+		{
+			auto err = GetLastError();
+			assert(false && "Creating window failed");
+			return -1;
+		}
+		m_hMainWnd = hWnd;
+
+		// display the window
+		ShowWindow(hWnd, SW_SHOW);
+		return 0;
+	}
 };
 
 int WINAPI wWinMain(
@@ -179,7 +323,8 @@ int WINAPI wWinMain(
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR lpCmdLine,
 	_In_ int nShowCmd
-) {
+)
+{
 #if _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	// watch Visual Studio's output window for memory leak messages
@@ -195,50 +340,17 @@ int WINAPI wWinMain(
 	// then set break point using _CrtSetBreakAlloc, NOTE that the "249" comes from the output message
 	//_CrtSetBreakAlloc(249);
 #endif
-	// create the window class
-	WNDCLASSEX wc{};
-	wc.cbSize = sizeof(WNDCLASSEX);
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WindowProc;
-	wc.hInstance = hInstance;
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wc.lpszClassName = _T("WIndowClass1");  // NOTE that spell mistake, this name is used for create the window
-
-	RegisterClassEx(&wc);
-
-	HWND hWnd;
-	if (!(hWnd = CreateWindowEx(
-		0,
-		_T("WIndowClass1"),
-		_T("lpWindowName"),
-		WS_OVERLAPPEDWINDOW,
-		300, 300,
-		500, 400,
-		nullptr,
-		nullptr,
+	g_pApp = new D3DApp(
 		hInstance,
-		nullptr)))
-		assert(false && "Creating window failed");
+		hPrevInstance,
+		lpCmdLine,
+		nShowCmd);
 
-	// display the window
-	ShowWindow(hWnd, nShowCmd);
-
-	// do init here
-	Initialize(hWnd);
-
-	// wait for the next msg in the queue
-	MSG msg{};
-	while (GetMessage(&msg, nullptr, 0, 0)) {
-		// translate msg into the right form
-		TranslateMessage(&msg);
-		// call WindowProc callback
-		DispatchMessage(&msg);
+	if (g_pApp->Initialize())
+	{
+		assert(false && "");
+		return -1;
 	}
 
-	// do cleanup here
-	Finalize();
-
-	// return WM_QUIT ???
-	return msg.wParam;
+	return g_pApp->Run();
 }
