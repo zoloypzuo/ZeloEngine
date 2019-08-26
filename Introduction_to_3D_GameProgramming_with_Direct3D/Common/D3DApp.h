2d3e7a68ac9516a62b2f29226bf3f619fd7d4857
zoloypzuo <zoloypzuo@tencent.com>
2019-08-26 20:42:28 +0800

    remove unused link lib code in D3DApp

--- Introduction_to_3D_GameProgramming_with_Direct3D/Common/D3DApp.h
+++ Introduction_to_3D_GameProgramming_with_Direct3D/Common/D3DApp.h
@@ -16,21 +16,6 @@
 #include <DirectXColors.h>
 #include <DirectXPackedVector.h>
 
-// dxut lib
-
-//Severity	Code	Description	Project	File	Line	Suppression State
-//Error	LNK2019	unresolved external symbol "long __stdcall DXUTTrace(char const *,unsigned long,long,wchar_t const *,bool)" (? DXUTTrace@@YGJPBDKJPB_W_N@Z) referenced in function "void __cdecl Initialize(struct HWND__ *)" (? Initialize@@YAXPAUHWND__@@@Z)	Lesson2_DirectX_Init	D : \ZeloEngine\build\Lesson2_DirectX_Init.obj	1
-//#pragma comment(lib, "DXUT.lib")
-//#pragma comment(lib, "DXUTOpt.lib")
-//#pragma comment(lib, "comctl32.lib")
-//#pragma comment(lib, "d3dcompiler.lib")
-//#pragma comment(lib, "usp10.lib")
-//#pragma comment(lib, "dxguid.lib")
-//#pragma comment(lib, "winmm.lib")
-//Severity	Code	Description	Project	File	Line	Suppression State
-//Error	LNK2019	unresolved external symbol _D3D11CreateDeviceAndSwapChain@48 referenced in function "void __cdecl Initialize(struct HWND__ *)" (? Initialize@@YAXPAUHWND__@@@Z)	Lesson2_DirectX_Init	D : \ZeloEngine\build\Lesson2_DirectX_Init.obj	1
-//#pragma comment(lib, "D3D11.lib")
-
 #define UNICODE
 #define DXUT_AUTOLIB
 #include "DXUT.h"
@@ -102,12 +87,13 @@ public:
 
 	int Initialize();
 
+	void Finalize();
+
 	int Run();
 
 protected:
 	int InitDirect3D();
 
-	void Finalize();
 
 	void RenderFrame();
 
