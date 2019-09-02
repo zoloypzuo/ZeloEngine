602d64b2c9de663d71d5b3b9508e19af28204335
mario <910181598@qq.com>
2019-09-02 16:58:30 +0800

    create LogModule

---
+++ Introduction_to_3D_GameProgramming_with_Direct3D/Common/LogModule/guicon.cpp
@@ -0,0 +1,9 @@
+// guicon.cpp
+// created on 2019/9/2
+// author @zoloypzuo
+
+//
+// Created by 91018 on 2019/9/2.
+//
+
+#include "guicon.h"
---
+++ Introduction_to_3D_GameProgramming_with_Direct3D/Common/LogModule/guicon.h
@@ -0,0 +1,18 @@
+// guicon.h
+// created on 2019/9/2
+// author @zoloypzuo
+
+//
+// Created by 91018 on 2019/9/2.
+//
+
+#ifndef ZELOENGINE_GUICON_H
+#define ZELOENGINE_GUICON_H
+
+
+class guicon {
+
+};
+
+
+#endif //ZELOENGINE_GUICON_H
