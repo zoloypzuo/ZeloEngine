67ecc6195b7f5f6df64c3fa473998cf79b905db5
mario <910181598@qq.com>
2019-09-02 19:05:29 +0800

    add clean.bat

---
+++ clean.bat
@@ -0,0 +1,19 @@
+@rem clean.bat
+@rem created on 2019/9/2
+@rem author @zoloypzuo
+@rem
+@rem rmdir build
+
+@rem uncomment this to log more and help debug
+rem @echo off
+
+set ScriptDir=%~dp0
+set Args=%*
+
+@rem cd root dir
+cd /d %ScriptDir%
+
+@rem rmdir is not necessary unless you make some mistake in build directory
+rmdir /s/q build
+
+pause
