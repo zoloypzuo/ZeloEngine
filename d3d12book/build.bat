64683878038b8a07351cda38b34111a99d4b4483
mario <910181598@qq.com>
2019-09-13 15:37:59 +0800

    refactor bat, build dx12book
    Former-commit-id: 37a94e73281061c2da97e21f0baa30b1d14b93db

---
+++ d3d12book/build.bat
@@ -0,0 +1,13 @@
+@rem build.bat
+@rem created on 2019/9/13
+@rem author @zoloypzuo
+@rem
+@rem build dxd12book
+
+set ScriptDir=%~dp0
+cd /d %ScriptDir%
+
+mkdir build
+cd build
+cmake -G "Visual Studio 15" ..
+msbuild dxd12book.sln
