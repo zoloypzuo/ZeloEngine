33635bf1c65f2f54967de616666a18106dc2a0d3
mario <910181598@qq.com>
2019-09-14 21:47:41 +0800

    Delete load_vs2017_devcmd.bat
    Former-commit-id: a1130e37c78c8ae1b1a8d3d2dad2f9f101a69091

--- load_vs2017_devcmd.bat
+++
@@ -1,9 +0,0 @@
-@rem copy this to where you want to load vs2017 devcmd, and call it by git bash
-
-set ScriptDir=%~dp0
-
-cd /d "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools"
-
-call VsDevCmd.bat
-
-cd /d %ScriptDir%
