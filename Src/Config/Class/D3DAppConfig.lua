3dcaaa4891129d878514ac2eed875361d831179d
mario <910181598@qq.com>
2019-09-15 20:52:04 +0800

    Update D3DAppConfig.lua

--- Src/Config/Class/D3DAppConfig.lua
+++ Src/Config/Class/D3DAppConfig.lua
@@ -2,20 +2,22 @@
 -- created on 2019/8/26
 -- author @zoloypzuo
 
+require("../../Script/PlainClass")
 local D3D_DRIVER_TYPE_HARDWARE = 1
 
-D3DAppConfig = {
+local D3DAppConfig = PlainClass(function(self)
     -- some window configurations
-    mainWndCaption = "D3D11 App",
-    clientWidth = 800,
-    clientHeight = 600,
+    self.mainWndCaption = "D3D11 App"
+    self.clientWidth = 800
+    self.clientHeight = 600
 
     -- some Direct3D configurations
-    driverType = D3D_DRIVER_TYPE_HARDWARE,
-    enable4xMsaa = false,
-    _4xMsaaQuality = 0,
+    self.driverType = D3D_DRIVER_TYPE_HARDWARE
+    self.enable4xMsaa = false
+    self._4xMsaaQuality = 0
 
-    engineDir = [[E:/ZeloEngineRoot/ZeloEngine/Introduction_to_3D_GameProgramming_with_Direct3D/]],
-    configDir = [[E:/ZeloEngineRoot/ZeloEngine/Introduction_to_3D_GameProgramming_with_Direct3D/]] .. "Config/"
-}
+    self.engineDir = [[E:/ZeloEngineRoot/ZeloEngine/Introduction_to_3D_GameProgramming_with_Direct3D/]]
+    self.configDir = [[E:/ZeloEngineRoot/ZeloEngine/Introduction_to_3D_GameProgramming_with_Direct3D/]] .. "Config/"
+end)
 
+return D3DAppConfig
