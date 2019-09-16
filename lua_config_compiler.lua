456e1220dc11e48368cb81f21e372f0e6df233ce
mario <910181598@qq.com>
2019-09-16 10:06:19 +0800

    unfinished commit
    Former-commit-id: a00c88bbdaf7ece24477887348b50ae360b358e0

---
+++ lua_config_compiler.lua
@@ -0,0 +1,33 @@
+-- lua_config_compiler.lua
+-- created on 2019/9/14
+-- author @zoloypzuo
+--
+-- see Src/Config/D3DAppConfig as example
+-- 1. load lua code, reflect and generate cpp code, NOTE to check config error
+-- 2. copy generated cpp code to Src/Config dir
+-- [x] update CMakeList.txt, make sure the cpp code is added to cmake: cmake will handle that
+--
+-- see design doc at github Lua配置管理的设计 #97
+--
+-- why it is called "compiler"? because it generate cpp code, and cpp code need to be compiled
+
+-- foreach lua file in Class dir, require it
+-- read its name and table
+-- foreach item in the table, get name and type, check type is number (int and float), or string, or bool
+-- generate a Cpp struct
+-- generate load code, it is HARD
+
+local config_dir = "Src/Config/"
+local config_class_dir = "Src/Config/Class/"
+
+local function handle_cls(classname)
+    local cls = require(classname)
+    local o = cls()
+    for k, v in pairs(o) do
+        local t = type(v)
+        if t == "number" then
+        elseif t == "string" then
+        elseif t == "boolean" then
+        end
+    end
+end
