217339786dd7c1d74e043897025394b6c6a42d5c
mario <910181598@qq.com>
2019-09-23 22:16:53 +0800

    add Global to ZeloEngine
    Former-commit-id: bca21999d0f14f461e949398bd9a50c837e87e5b

---
+++ Src/Common/Global.cpp
@@ -0,0 +1,5 @@
+// Global.cpp
+// created on 2019/9/23
+// author @zoloypzuo
+
+#include "Global.h"
---
+++ Src/Common/Global.h
@@ -0,0 +1,16 @@
+// Global.h
+// created on 2019/9/23
+// author @zoloypzuo
+
+#ifndef ZELOENGINE_GLOBAL_H
+#define ZELOENGINE_GLOBAL_H
+
+
+class Global {
+
+};
+
+const char* k_engineRootDir = R"(D:\ZeloEngine\)";
+
+
+#endif //ZELOENGINE_GLOBAL_H
