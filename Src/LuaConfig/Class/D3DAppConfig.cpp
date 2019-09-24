28e349952e60f5ed6dfd78e6fe08a86616d26ab6
mario <910181598@qq.com>
2019-09-25 00:52:07 +0800

    finish LuaConfigManager
    Former-commit-id: 61a0b6ea3a6d425b3388686329f65fb497a0cddb

--- Src/LuaConfig/Class/D3DAppConfig.cpp
+++ Src/LuaConfig/Class/D3DAppConfig.cpp
@@ -11,56 +11,11 @@ D3DAppConfig::D3DAppConfig()
 	// do nothing
 }
 
-int D3DAppConfig::LoadConfig(lua_State* L, D3DAppConfig** ppConfig)
+void D3DAppConfig::LoadConfig(lua_State* L, D3DAppConfig* pConfig)
 {
-	lua_pushcfunction(L, &pLoadConfig);
-	if (lua_pcall(L, 0, 1, 0))
-	{
-		fprintf(stderr, "%s", lua_tostring(L, LUA_TOP));
-		lua_pop(L, 1);
-		return -1;
-	}
-	else
-	{
-		if (!lua_isuserdata(L, LUA_TOP))
-		{
-			fprintf(stderr, "is not ud");
-			lua_pop(L, 1);
-			return -1;
-		}
-		*ppConfig = (D3DAppConfig*)lua_touserdata(L, LUA_TOP);
-	}
-	return 0;
-}
+	// Õ»¶¥ÊÇluaÅäÖÃ¶ÔÏó
+	luaL_checktype(L, LUA_TOP, LUA_TTABLE);
 
-// D3DAppConfig LoadConfig()
-int D3DAppConfig::pLoadConfig(lua_State* L)
-{
-	stackDump(L);
-	HRESULT hr; // used for V
-
-	// read file to buffer
-	ID3DBlob* pContent{};
-	//V(D3DReadFileToBlob(L"../../LuaConfig/D3DAppConfig.lua", &pContent));
-
-	// dostring
-	auto buffer = pContent->GetBufferPointer();
-	auto size = pContent->GetBufferSize();
-	if (luaL_loadbuffer(L, (char*)buffer, size, "") || docall(L, 0, 0))
-	{
-		// get err msg from stack top
-		luaL_error(L, "cannot run config file: %s", lua_tostring(L, LUA_TOP));
-	}
-
-	D3DAppConfig* pConfig = new D3DAppConfig();
-	lua_pushlightuserdata(L, pConfig);
-
-	lua_getglobal(L, "D3DAppConfig");
-
-	if (!lua_istable(L, LUA_TOP))
-	{
-		return luaL_error(L, "D3DAppConfig is not a table");
-	}
 	pConfig->mainWndCaption = getFieldString(L, "mainWndCaption");
 	pConfig->clientWidth = (int)getFieldInt(L, "clientWidth");
 	pConfig->clientHeight = (int)getFieldInt(L, "clientHeight");
@@ -71,13 +26,4 @@ int D3DAppConfig::pLoadConfig(lua_State* L)
 	pConfig->configDir = getFieldString(L, "configDir");
 
 	lua_pop(L, 1); // pop D3DAppConfig
-
-	lua_pushnil(L);
-	lua_setglobal(L, "D3DAppConfig");
-	if (lua_getglobal(L, "D3DAppConfig") != LUA_TNIL)
-	{
-		luaL_error(L, "D3DAppConfig is not set to nil");
-	}
-	lua_pop(L, 1);
-	return 1;
 }
--- Src/Module/LuaModule/LuaConfigManager.h
+++ Src/Module/LuaModule/LuaConfigManager.h
@@ -5,18 +5,74 @@
 #ifndef ZELOENGINE_LUACONFIGMANAGER_H
 #define ZELOENGINE_LUACONFIGMANAGER_H
 #include <string>
-
+#include "Zelo.h"
+#include "LuaUtil.h"
+#include "GlobalConfig.h"
 
 class LuaConfigManager {
 public:
-    template<typename T>
-    T* LookupSymbol(const char* name);
+	/**
+	 * \brief 加载配置文件（返回一个Lua表，代表一个配置对象），返回一个配置对象
+	 */
+	template<typename T>
+	T* LoadConfig(const char* name);
+
+	/**
+	 * \brief 加载配置文件（返回一个Lua表，代表一个配置对象的列表），返回一个配置对象列表
+	 */
+	template<typename T>
+	T** LoadConfigList(const char* name);
 };
 
 template <typename T>
-T* LuaConfigManager::LookupSymbol(const char* name)
+T* LuaConfigManager::LoadConfig(const char* name)
 {
-	return T::LoadConfig();
+	lua_pushcfunction(L, [](L)->int
+	{
+		if (luaL_dofile(L, (g_configFileDir + name).c_str()) != LUA_OK)
+		{
+			luaL_error("cannot run config file");
+		}
+		luaL_checktype(L, LUA_TOP, LUA_TTABLE);
+		T* pConfig = new T();
+		T::LoadConfig(L, pConfig);
+		lua_pushlightuserdata(L, pConfig);
+		return 1;
+	});
+	//lua_pushstring(L, name); c++ lambda可以闭包
+	docall(L, 0);
+	T* ret = lua_touserdata(L, LUA_TOP);
+	// lua配置脚本最后一句是return一个表，加载完了要弹掉
+	lua_pop(L, 1);
+	return ret;
+}
+
+template <typename T>
+T** LuaConfigManager::LoadConfigList(const char* name)
+{
+	lua_pushcfunction(L, [](L)->int
+	{
+		if (luaL_dofile(L, (g_configFileDir + name).c_str()) != LUA_OK)
+		{
+			luaL_error("cannot run config file");
+		}
+		luaL_checktype(L, LUA_TOP, LUA_TTABLE);
+		lua_Integer len = luaL_len(L, LUA_TOP);
+		T** pConfig = new T[len];
+		for (size_t i = 0; i < len; i++)
+		{
+			lua_geti(L, LUA_TOP, i);
+			T::LoadConfig(L, pConfig);
+		}
+		lua_pushlightuserdata(L, pConfig);
+		return 1;
+	});
+	//lua_pushstring(L, name); c++ lambda可以闭包
+	docall(L, 0);
+	T** ret = lua_touserdata(L, LUA_TOP);
+	// lua配置脚本最后一句是return一个表，加载完了要弹掉
+	lua_pop(L, 1);
+	return ret;
 }
 
 
