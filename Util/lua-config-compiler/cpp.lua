28a97f15042aa9debffdce3de924b86b41eb5f3a
mario <910181598@qq.com>
2019-09-26 22:15:52 +0800

    fix #13

--- Util/lua-config-compiler/cpp.lua
+++ Util/lua-config-compiler/cpp.lua
@@ -53,6 +53,42 @@ function member_var_decl(type, name)
     return list(typemap[type] .. " " .. name .. "{};\n")
 end
 
+function generate_param_list(param_type_name_list)
+    -- in {{lua_State*, L}, {D3DAppConfig*, pConfig}}
+    -- out "lua_State* L, D3DAppConfig* pConfig"
+    return table.concat(map(function(t)
+        return table.concat(t, " ")
+    end, param_type_name_list), ", ")
+end
+
+function impl_member_function(ret_type, classname, method_name, param_type_name_list, code)
+    return list(ret_type .. " " .. classname .. "::" .. method_name .. "(" ..
+            generate_param_list(param_type_name_list) .. ")\n{\n") ..
+            tab(code)
+            .. list("}\n")
+end
+
+function empty_ctor(classname)
+    return list(classname .. "::" .. classname .. "()\n" .. [[
+{
+}
+]])
+end
+
+-- 赋值语句，左值和右值，这里简化为字符串，避免复杂化
+function assign_stat(lvar, rvar)
+    return list(lvar .. " = " .. rvar .. ";\n")
+end
+
+-- 函数调用表达式（注意不带分号），arglist是字符串列表，同样是简化了
+function function_call_exp(funcname, arglist)
+    return funcname .. "(" .. table.concat(arglist, ", ") .. ")"
+end
+
+-- 转换为cpp字符串字面量，暂时不考虑转义
+function string_literal(s)
+    return "\"" .. s .. "\""
+end
 
 --print(header_guard("AAA", list()))
 --print(include("lua.hpp"))
