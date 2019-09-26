04804bb1761d676f8e67b3f5dc94d6210a0d0e64
mario <910181598@qq.com>
2019-09-26 14:22:58 +0800

    Update cpp.lua
    Former-commit-id: 66f2fb2169c6107558d69f79a0720c670f9305d4

--- Util/lua-config-compiler/cpp.lua
+++ Util/lua-config-compiler/cpp.lua
@@ -1,3 +1,27 @@
 -- cpp.lua
 -- created on 2019/9/26
 -- author @zoloypzuo
+
+require("global")
+
+function tab(code)
+    local ret = {}
+    for i, line in ipairs(code) do
+        ret[i] = "\t" + line
+    end
+    return ret
+end
+
+function join(code)
+    return table.concat(code)
+end
+
+function header_guard(classname, code)
+    local start = [[
+#ifndef ZELOENGINE_D3DAPPCONFIG_H
+#define ZELOENGINE_D3DAPPCONFIG_H]]
+    local _end = [[#endif //ZELOENGINE_D3DAPPCONFIG_H]]
+    start = string.gsub(start, "D3DAPPCONFIG", classname)
+    _end = string.gsub(_end, "D3DAPPCONFIG", classname)
+    return list.append()
+end
