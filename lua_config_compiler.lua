4c46274f05c192e7668eb4386d7572c2dbf55c83
mario <910181598@qq.com>
2019-09-26 09:06:20 +0800

    unfinished lua config compiler

--- lua_config_compiler.lua
+++ lua_config_compiler.lua
@@ -17,17 +17,43 @@
 -- generate a Cpp struct
 -- generate load code, it is HARD
 
-local config_dir = "Src/LuaConfig/"
-local config_class_dir = "Src/LuaConfig/Class/"
+local k_config_dir = [[D:\ZeloEngine\Src\LuaConfig]]
+local k_lua_config_class_dir = [[D:\ZeloEngine\Src\LuaConfig\LuaConfigClass\]]  -- 实验了一下，正反斜杠对lfs都是ok的
+local k_cpp_config_class_dir = [[D:\ZeloEngine\Src\LuaConfig\CppConfigClass_Generated]]
 
-local function handle_cls(classname)
-    local cls = require(classname)
-    local o = cls()
-    for k, v in pairs(o) do
-        local t = type(v)
-        if t == "number" then
-        elseif t == "string" then
-        elseif t == "boolean" then
+local lfs = require 'lfs'
+
+-- 区分出int和float
+local function ex_type(o)
+    local t = type(o)
+    if t == "number" then
+        return math.type(o)
+    else
+        return type(o)
+    end
+end
+
+local function header_guard(classname)
+
+end
+
+local function generate_cpp_code(classname, symbol_table)
+    local header_code = {}
+    local cpp_code = {}
+
+end
+
+for filename in lfs.dir(k_lua_config_class_dir) do
+    local path = k_lua_config_class_dir .. filename
+    if lfs.attributes(path, "mode") == "file" then
+        -- is file?
+        local classname = assert(string.match(filename, "([%a_][%w_]*)%.lua"))
+        local cls = dofile(filename)
+        local o = cls()
+        local symbol_table = {}
+        for k, v in pairs(o) do
+            symbol_table[k] = ex_type(v)
+            generate_cpp_code(classname, symbol_table)
         end
     end
 end
