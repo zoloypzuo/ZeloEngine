cbb4b8738ef16d3f723b0bfe2860629f89040612
mario <910181598@qq.com>
2019-09-27 17:38:16 +0800

    add Common, remove LogModule, fix #15

---
+++ Src/Common/Common.cpp
@@ -0,0 +1,5 @@
+// Common.cpp
+// created on 2019/9/27
+// author @zoloypzuo
+
+#include "Common.h"
---
+++ Src/Common/Common.h
@@ -0,0 +1,15 @@
+// Common.h
+// created on 2019/9/27
+// author @zoloypzuo
+
+#ifndef ZELOENGINE_COMMON_H
+#define ZELOENGINE_COMMON_H
+
+#include "spdlog/spdlog.h"
+using namespace spdlog;
+
+#include "GlobalConfig.h"
+#include "d3dApp.h"
+#include "MathHelper.h"
+
+#endif //ZELOENGINE_COMMON_H
