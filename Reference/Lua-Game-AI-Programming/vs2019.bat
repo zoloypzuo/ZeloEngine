b4e668e1f8be5075555f88aac46778bd29dabb83
zoloypzuo <910181598@qq.com>
2019-10-07 00:37:57 +0800

    fix #147

--- Reference/Lua-Game-AI-Programming/vs2019.bat
+++ Reference/Lua-Game-AI-Programming/vs2019.bat
@@ -7,11 +7,11 @@ cd /d %SCRIPT_DIRECTORY%
 
 
 @rem copy Sandbox Resource config files to bin
-xcopy config\ bin\ /s/e /y
+xcopy config bin /s/e /y
 
 echo on
 
-tools\premake\premake5 --os=windows --file=premake/premake.lua vs2019 %ARGUMENTS%
+tools\premake\premake5 --os=windows --file=premake/premake.lua vs2013 %ARGUMENTS%
 
 cd /d %CURRENT_DIRECTORY%
 
