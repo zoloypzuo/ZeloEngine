bddc80ea782bf43d7860e69fd92b61ab7b0ce5d2
zoloypzuo <910181598@qq.com>
2019-10-07 20:43:43 +0800

    Update prefabs.lua
    Former-commit-id: 90e3fb9368c6e90a9b423fbc9d36307a1c9d3c10

--- Reference/DontStarveScript/prefabs.lua
+++ Reference/DontStarveScript/prefabs.lua
@@ -1,7 +1,14 @@
+-- Prefab.lua
+-- 2019年10月7日
+
 require("class")
 --require("entityscript")
 --PREFABS.LUA
 
+-- 名字
+-- 函数
+-- 资产列表
+-- 深度？
 Prefab = Class( function(self, name, fn, assets, deps)
     self.name = name or ""
     self.path = name or nil
@@ -16,6 +23,9 @@ function Prefab:__tostring()
     return string.format("Prefab %s - %s", self.name, self.desc)
 end
 
+-- 类型
+-- 文件
+-- 参数
 Asset = Class( function(self, type, file, param)
     self.type = type
     self.file = file
