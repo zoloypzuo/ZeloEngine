d20c8ff695b8d4c018defeeccb87b6b2f972191c
zoloypzuo <910181598@qq.com>
2019-10-08 20:15:37 +0800

    《Lua-Game-AI》第七章

--- Reference/Lua-Game-AI-Programming/src/chapter_7_knowledge/script/SoldierEvaluators.lua
+++ Reference/Lua-Game-AI-Programming/src/chapter_7_knowledge/script/SoldierEvaluators.lua
@@ -1,25 +1,3 @@
---[[
-  Copyright (c) 2013 David Young dayoung@goliathdesigns.com
-
-  This software is provided 'as-is', without any express or implied
-  warranty. In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-   1. The origin of this software must not be misrepresented; you must not
-   claim that you wrote the original software. If you use this software
-   in a product, an acknowledgment in the product documentation would be
-   appreciated but is not required.
-
-   2. Altered source versions must be plainly marked as such, and must not be
-   misrepresented as being the original software.
-
-   3. This notice may not be removed or altered from any source
-   distribution.
-]]
 
 --[[
 userData =
@@ -32,20 +10,20 @@ userData =
 
 function SoldierEvaluators_CanShootAgent(userData)
     local enemy = userData.blackboard:Get("enemy");
-    
+
     if (enemy ~= nil and
-        -- A distance check to determine whether one Agent can
-        -- shoot another.
-        Agent.GetHealth(enemy) > 0 and
-        Vector.Distance(userData.agent:GetPosition(), enemy:GetPosition()) < 3) then
+            -- A distance check to determine whether one Agent can
+            -- shoot another.
+            Agent.GetHealth(enemy) > 0 and
+            Vector.Distance(userData.agent:GetPosition(), enemy:GetPosition()) < 3) then
         return true;
-    end;
+    end ;
     return false;
 end
 
 function SoldierEvaluators_HasAmmo(userData)
     local ammo = userData.blackboard:Get("ammo");
-    
+
     return ammo ~= nil and ammo > 0;
 end
 
@@ -71,7 +49,7 @@ function SoldierEvaluators_HasMovePosition(userData)
     local movePosition = userData.agent:GetTarget();
 
     return movePosition ~= nil and
-        (Vector.Distance(userData.agent:GetPosition(), movePosition) > 1.5);
+            (Vector.Distance(userData.agent:GetPosition(), movePosition) > 1.5);
 end
 
 function SoldierEvaluators_IsAlive(userData)
