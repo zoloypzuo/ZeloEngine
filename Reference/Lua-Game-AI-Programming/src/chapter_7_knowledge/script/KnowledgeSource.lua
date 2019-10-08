36de9286ea6043140c2702bd6b2f0f92848af887
zoloypzuo <910181598@qq.com>
2019-10-08 20:15:37 +0800

    《Lua-Game-AI》第七章
    Former-commit-id: d20c8ff695b8d4c018defeeccb87b6b2f972191c

--- Reference/Lua-Game-AI-Programming/src/chapter_7_knowledge/script/KnowledgeSource.lua
+++ Reference/Lua-Game-AI-Programming/src/chapter_7_knowledge/script/KnowledgeSource.lua
@@ -1,26 +1,14 @@
---[[
-  Copyright (c) 2013 David Young dayoung@goliathdesigns.com
+-- KnowledgeSource.lua
+-- 2019年10月8日
 
-  This software is provided 'as-is', without any express or implied
-  warranty. In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-   1. The origin of this software must not be misrepresented; you must not
-   claim that you wrote the original software. If you use this software
-   in a product, an acknowledgment in the product documentation would be
-   appreciated but is not required.
-
-   2. Altered source versions must be plainly marked as such, and must not be
-   misrepresented as being the original software.
-
-   3. This notice may not be removed or altered from any source
-   distribution.
-]]
-
+-- 知识源
+-- 代表一个需要复杂计算得到知识的源信息，数据
+-- 缓存计算和更新
+-- 复杂计算会被缓存，并按指定的频率重新计算
+-- （AI的思考频率不必是每帧一次，但是像王者这样复杂紧张的战斗，频率要求可能比较高）
+--
+-- 计算置信度
+-- 置信度是0~1的值，也就是概率，置信度高，说明AI应该更加相信它
 KnowledgeSource = {};
 
 function KnowledgeSource.Evaluate(self, userData)
@@ -30,27 +18,26 @@ function KnowledgeSource.Evaluate(self, userData)
     -- Wait till enough time has passed to re-evaluate the knowledge source.
     if (nextUpdateTime > self.lastUpdateTime_) then
         self.lastUpdateTime_ = time;
-        
         local result = self.evaluator_(userData);
         self.confidence_ = result.confidence;
         self.evaluation_ = result.evaluation;
     end
-    
+
     return { evaluation = self.evaluation_, confidence = self.confidence_ };
 end
 
 function KnowledgeSource.new(evaluator, updateFrequency)
     local source = {};
-    
+
     -- The KnowledgeSource's data members.
     source.confidence_ = 0;
     source.evaluation_ = nil;
     source.evaluator_ = evaluator;
     source.lastUpdateTime_ = 0;
     source.updateFrequency_ = updateFrequency or 0;
-    
+
     -- The KnowledgeSource's accessor functions.
     source.Evaluate = KnowledgeSource.Evaluate;
-    
+
     return source;
 end
