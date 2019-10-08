e6b76eeb1d1376f445ccba3fa2dca1ffcd416579
zoloypzuo <910181598@qq.com>
2019-10-08 20:15:37 +0800

    《Lua-Game-AI》第七章
    Former-commit-id: d6f373d809399102e8527ae46e0219b6ea533ea4

--- Reference/Lua-Game-AI-Programming/src/chapter_7_knowledge/script/SoldierKnowledge.lua
+++ Reference/Lua-Game-AI-Programming/src/chapter_7_knowledge/script/SoldierKnowledge.lua
@@ -1,25 +1,3 @@
---[[
-  Copyright (c) 2013 David Young dayoung@goliathdesigns.com
-
-  This software is provided 'as-is', without any express or implied
-  warranty. In no event will the authors be held liable for any damages
-  arising from the use of this software.
-
-  Permission is granted to anyone to use this software for any purpose,
-  including commercial applications, and to alter it and redistribute it
-  freely, subject to the following restrictions:
-
-   1. The origin of this software must not be misrepresented; you must not
-   claim that you wrote the original software. If you use this software
-   in a product, an acknowledgment in the product documentation would be
-   appreciated but is not required.
-
-   2. Altered source versions must be plainly marked as such, and must not be
-   misrepresented as being the original software.
-
-   3. This notice may not be removed or altered from any source
-   distribution.
-]]
 
 require "Soldier"
 
@@ -31,19 +9,18 @@ function SoldierKnowledge_ChooseBestEnemy(userData)
     local closestEnemy;
     local distanceToEnemy;
 
-    for index=1, #agents do
+    for index = 1, #agents do
         local agent = agents[index];
 
         -- Only check against living agents.
         if (Agent.GetId(agent) ~= Agent.GetId(userData.agent) and
-            Agent.GetHealth(agent) > 0) then
+                Agent.GetHealth(agent) > 0) then
 
-            local distanceToAgent =
-                Vector.DistanceSquared(position, agent:GetPosition());
+            local distanceToAgent = Vector.DistanceSquared(position, agent:GetPosition());
 
             if (closestEnemy == nil or distanceToAgent < distanceToEnemy) then
                 local path = Sandbox.FindPath(
-                    sandbox, "default", position, agent:GetPosition());
+                        sandbox, "default", position, agent:GetPosition());
 
                 -- Make sure the enemy is pathable.
                 if (#path ~= 0) then
@@ -53,7 +30,7 @@ function SoldierKnowledge_ChooseBestEnemy(userData)
             end
         end
     end
-    
+
     return { confidence = 1, evaluation = closestEnemy };
 end
 
@@ -70,11 +47,10 @@ function SoldierKnowledge_ChooseBestFleePosition(userData)
 
         -- Try 32 points, and pick the furthest point away from the current
         -- enemy.
-        for index=1, 32 do
+        for index = 1, 32 do
             position = Sandbox.RandomPoint(sandbox, "default");
 
-            local distanceToAgent =
-                Vector.DistanceSquared(position, enemyPosition);
+            local distanceToAgent = Vector.DistanceSquared(position, enemyPosition);
 
             if (bestDistance < distanceToAgent) then
                 bestDistance = distanceToAgent;
@@ -85,6 +61,6 @@ function SoldierKnowledge_ChooseBestFleePosition(userData)
         -- Any position will work if the agent doesn't have an enemy.
         bestPosition = Soldier_PathToRandomPoint(userData.agent);
     end
-    
+
     return { confidence = 1, evaluation = bestPosition };
 end
