1f607cd0f13f213a681b2d28a84a1f0eb3e4404a
zoloypzuo <910181598@qq.com>
2019-10-09 14:12:55 +0800

    move demo_framework to SandboxFramework

---
+++ Src/SandboxFramework/Script/AnimationState.lua
@@ -0,0 +1,20 @@
+AnimationState = {};
+
+function AnimationState.new()
+    local state = {};
+
+    -- Contains the Ogre AnimationState animation.
+    state.animation_ = nil;
+
+    -- The name of the animation state, this must be unique within the
+    -- animation state machine.
+    state.name_ = "";
+
+    -- Whether the animation is looping.
+    state.looping_ = false;
+
+    -- How fast the animation should play.
+    state.rate_ = 1;
+
+    return state;
+end
