d789bd0f480dfa53ce3180ed9069152451f6893f
zoloypzuo <910181598@qq.com>
2019-10-09 13:18:03 +0800

    add Init_OgreApp
    Former-commit-id: fd841bc236dcc308e34e850e3bfaafe3cc7d2155

---
+++ Example/Init-OgreApp/Init_OgreApp.h
@@ -0,0 +1,14 @@
+// Init_OgreApp.h
+// created on 2019/10/9
+// author @zoloypzuo
+
+#ifndef ZELOENGINE_INIT_OGREAPP_H
+#define ZELOENGINE_INIT_OGREAPP_H
+
+
+class Init_OgreApp {
+
+};
+
+
+#endif //ZELOENGINE_INIT_OGREAPP_H
