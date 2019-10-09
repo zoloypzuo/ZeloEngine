f51c9cba7e6be7d3b06c11e96927d8fbf6bfb931
zoloypzuo <910181598@qq.com>
2019-10-09 14:12:55 +0800

    move demo_framework to SandboxFramework
    Former-commit-id: 1f607cd0f13f213a681b2d28a84a1f0eb3e4404a

---
+++ Src/SandboxFramework/Script/SoldierEvaluators.lua
@@ -0,0 +1,68 @@
+--[[
+userData =
+{
+    agent, -- Sandbox Agent
+    blackboard,  -- Agent Blackboard
+    controller, -- Agent Controller
+};
+]]
+
+function SoldierEvaluators_CanShootAgent(userData)
+    local enemy = userData.blackboard:Get("enemy");
+
+    if (enemy ~= nil and
+            -- A simple distance check to determine whether one Agent can
+            -- shoot another.
+            Agent.GetHealth(enemy) > 0 and
+            Vector.Distance(userData.agent:GetPosition(), enemy:GetPosition()) < 3) then
+        return true;
+    end ;
+    return false;
+end
+
+function SoldierEvaluators_HasAmmo(userData)
+    local ammo = userData.blackboard:Get("ammo");
+
+    return ammo ~= nil and ammo > 0;
+end
+
+function SoldierEvaluators_HasNoAmmo(userData)
+    return not SoldierEvaluators_HasAmmo(userData);
+end
+
+function SoldierEvaluators_HasCriticalHealth(userData)
+    local maxHealth = userData.blackboard:Get("maxHealth");
+
+    return Agent.GetHealth(userData.agent) < (maxHealth * 0.2);
+end
+
+function SoldierEvaluators_HasEnemy(userData)
+    return userData.blackboard:Get("enemy") ~= nil;
+end
+
+function SoldierEvaluators_HasNoEnemy(userData)
+    return not SoldierEvaluators_HasEnemy(userData);
+end
+
+function SoldierEvaluators_HasMovePosition(userData)
+    local movePosition = userData.agent:GetTarget();
+
+    return movePosition ~= nil and
+            (Vector.Distance(userData.agent:GetPosition(), movePosition) > 1.5);
+end
+
+function SoldierEvaluators_IsAlive(userData)
+    return Agent.GetHealth(userData.agent) > 0;
+end
+
+function SoldierEvaluators_IsNotAlive(userData)
+    return not SoldierEvaluators_IsAlive(userData);
+end
+
+function SoldierEvaluators_Random(userData)
+    return math.random() >= 0.5;
+end
+
+function SoldierEvaluators_True(userData)
+    return true;
+end
