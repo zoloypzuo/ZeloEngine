c693a5ebcce1f0a2dc791370efdf1fa4b75096ea
zoloypzuo <910181598@qq.com>
2019-10-12 12:16:21 +0800

    create ogre app
    Former-commit-id: 3088240344231337e931f3f9ce7af6dc5b5e9a37 [formerly 196db93c424144561231f3d926a8b93a2130a4ff]
    Former-commit-id: ca4401b6c81eb156e2fb8a12d74cd4d32e5f2103

--- Example/Init-OgreApp/Init_OgreApp.cpp
+++ Example/Init-OgreApp/Init_OgreApp.cpp
@@ -3,3 +3,188 @@
 // author @zoloypzuo
 
 #include "Init_OgreApp.h"
+
+bool Init_OgreApp::frameStarted(const Ogre::FrameEvent& evt)
+{
+	return false;
+}
+
+bool Init_OgreApp::frameRenderingQueued(const Ogre::FrameEvent& evt)
+{
+	return false;
+}
+
+bool Init_OgreApp::frameEnded(const Ogre::FrameEvent& evt)
+{
+	return false;
+}
+
+void Init_OgreApp::setup()
+{
+	// do not forget to call the base first
+	OgreBites::ApplicationContext::setup();
+
+	// register for input events
+	addInputListener(this);
+	// get a pointer to the already created root
+	Ogre::Root* root = getRoot();
+	Ogre::SceneManager* scnMgr = root->createSceneManager();
+	// register our scene with the RTSS
+	Ogre::RTShader::ShaderGenerator* shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+	shadergen->addSceneManager(scnMgr);
+	// without light we would just get a black screen    
+	Ogre::Light* light = scnMgr->createLight("MainLight");
+	Ogre::SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+	lightNode->setPosition(0, 10, 15);
+	lightNode->attachObject(light);
+	// also need to tell where we are
+	Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+	camNode->setPosition(0, 0, 15);
+	camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+	// create the camera
+	Ogre::Camera* cam = scnMgr->createCamera("myCam");
+	cam->setNearClipDistance(5); // specific to this sample
+	cam->setAutoAspectRatio(true);
+	camNode->attachObject(cam);
+	// and tell it to render into the main window
+	getRenderWindow()->addViewport(cam);
+	// finally something to render
+	Ogre::Entity* ent = scnMgr->createEntity("Sinbad.mesh");
+	Ogre::SceneNode* node = scnMgr->getRootSceneNode()->createChildSceneNode();
+	node->attachObject(ent);
+}
+
+void Init_OgreApp::createRoot()
+{
+}
+
+bool Init_OgreApp::oneTimeConfig()
+{
+	return false;
+}
+
+void Init_OgreApp::loadResources()
+{
+}
+
+void Init_OgreApp::locateResources()
+{
+}
+
+void Init_OgreApp::shutdown()
+{
+}
+
+void Init_OgreApp::pollEvents()
+{
+}
+
+Init_OgreApp::~Init_OgreApp()
+{
+}
+
+void Init_OgreApp::frameRendered(const Ogre::FrameEvent& evt)
+{
+}
+
+bool Init_OgreApp::keyPressed(const OgreBites::KeyboardEvent& evt)
+{
+	return false;
+
+}
+
+bool Init_OgreApp::keyReleased(const OgreBites::KeyboardEvent& evt)
+{
+	return false;
+
+}
+
+bool Init_OgreApp::touchMoved(const OgreBites::TouchFingerEvent& evt)
+{
+	return false;
+
+}
+
+bool Init_OgreApp::touchPressed(const OgreBites::TouchFingerEvent& evt)
+{
+	return false;
+
+}
+
+bool Init_OgreApp::touchReleased(const OgreBites::TouchFingerEvent& evt)
+{
+	return false;
+
+}
+
+bool Init_OgreApp::mouseMoved(const OgreBites::MouseMotionEvent& evt)
+{
+	return false;
+
+}
+
+bool Init_OgreApp::mouseWheelRolled(const OgreBites::MouseWheelEvent& evt)
+{
+	return false;
+
+}
+
+bool Init_OgreApp::mousePressed(const OgreBites::MouseButtonEvent& evt)
+{
+	return false;
+
+}
+
+bool Init_OgreApp::mouseReleased(const OgreBites::MouseButtonEvent& evt)
+{
+	return false;
+}
+
+void Init_OgreApp::windowMoved(Ogre::RenderWindow* rw)
+{
+}
+
+void Init_OgreApp::windowResized(Ogre::RenderWindow* rw)
+{
+}
+
+bool Init_OgreApp::windowClosing(Ogre::RenderWindow* rw)
+{
+	return false;
+}
+
+void Init_OgreApp::windowClosed(Ogre::RenderWindow* rw)
+{
+}
+
+void Init_OgreApp::windowFocusChange(Ogre::RenderWindow* rw)
+{
+}
+
+void Init_OgreApp::reconfigure(const Ogre::String& renderer, Ogre::NameValuePairList& options)
+{
+}
+
+void Init_OgreApp::setWindowGrab(OgreBites::NativeWindowType* win, bool grab)
+{
+}
+
+void Init_OgreApp::addInputListener(OgreBites::NativeWindowType* win, InputListener* lis)
+{
+}
+
+void Init_OgreApp::removeInputListener(OgreBites::NativeWindowType* win, InputListener* lis)
+{
+}
+
+OgreBites::NativeWindowPair Init_OgreApp::createWindow(const Ogre::String& name, uint32_t w, uint32_t h,
+	Ogre::NameValuePairList miscParams)
+{
+	return {};
+}
+
+int main(size_t argc, char** argv)
+{
+	Init_OgreApp app;
+	return 0;
+}
--- Example/Init-OgreApp/Init_OgreApp.h
+++ Example/Init-OgreApp/Init_OgreApp.h
@@ -5,9 +5,47 @@
 #ifndef ZELOENGINE_INIT_OGREAPP_H
 #define ZELOENGINE_INIT_OGREAPP_H
 
+#include "Bites/OgreApplicationContext.h"
 
-class Init_OgreApp {
-
+class Init_OgreApp :OgreBites::ApplicationContext, public OgreBites::InputListener
+{
+public:
+	Init_OgreApp():OgreBites::ApplicationContext()
+	{
+		
+	}
+	bool frameStarted(const Ogre::FrameEvent& evt) override;
+	bool frameRenderingQueued(const Ogre::FrameEvent& evt) override;
+	bool frameEnded(const Ogre::FrameEvent& evt) override;
+	void setup() override;
+	void createRoot() override;
+	bool oneTimeConfig() override;
+	void loadResources() override;
+	void locateResources() override;
+	void shutdown() override;
+	void pollEvents() override;
+	~Init_OgreApp() override;
+	void frameRendered(const Ogre::FrameEvent& evt) override;
+	bool keyPressed(const OgreBites::KeyboardEvent& evt) override;
+	bool keyReleased(const OgreBites::KeyboardEvent& evt) override;
+	bool touchMoved(const OgreBites::TouchFingerEvent& evt) override;
+	bool touchPressed(const OgreBites::TouchFingerEvent& evt) override;
+	bool touchReleased(const OgreBites::TouchFingerEvent& evt) override;
+	bool mouseMoved(const OgreBites::MouseMotionEvent& evt) override;
+	bool mouseWheelRolled(const OgreBites::MouseWheelEvent& evt) override;
+	bool mousePressed(const OgreBites::MouseButtonEvent& evt) override;
+	bool mouseReleased(const OgreBites::MouseButtonEvent& evt) override;
+	void windowMoved(Ogre::RenderWindow* rw) override;
+	void windowResized(Ogre::RenderWindow* rw) override;
+	bool windowClosing(Ogre::RenderWindow* rw) override;
+	void windowClosed(Ogre::RenderWindow* rw) override;
+	void windowFocusChange(Ogre::RenderWindow* rw) override;
+	void reconfigure(const Ogre::String& renderer, Ogre::NameValuePairList& options) override;
+	void setWindowGrab(OgreBites::NativeWindowType* win, bool grab) override;
+	void addInputListener(OgreBites::NativeWindowType* win, InputListener* lis) override;
+	void removeInputListener(OgreBites::NativeWindowType* win, InputListener* lis) override;
+	OgreBites::NativeWindowPair createWindow(const Ogre::String& name, uint32_t w, uint32_t h,
+		Ogre::NameValuePairList miscParams) override;
 };
 
 
