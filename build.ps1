08264d9b0e218f9358c1d96bcbe43dd6582a1d3f
zoloypzuo <910181598@qq.com>
2019-10-12 13:48:18 +0800

    udpate build script
    Former-commit-id: 2309293c6fe16f0017cd5b8329e76082acd369e2

--- build.ps1
+++ build.ps1
@@ -1,91 +1,47 @@
-# @rem build.bat
-# @rem created on 2019/8/31
-# @rem author @zoloypzuo
+# build.ps1
+# created on 2019/8/31
+# author @zoloypzuo
 
-Write-Output @"
+function Initialize {
+    Write-Output @"
 =========================
 ==== build.ps1 start ====
 =========================
 "@
-$CurrentDir = Get-Location
-$ScriptDir = $PSScriptRoot
-Set-Location -Path $ScriptDir
+    $global:CurrentDir = Get-Location
+    $global:ScriptDir = $PSScriptRoot
+    Set-Location -Path $ScriptDir
+}
 
-Write-Output @"
-========================================
-==== set environment variable start ====
-========================================
+function Finialize {
+    Write-Output @"
+=========================
+==== build.ps1 end ====
+=========================
 "@
+    Set-Location $global:CurrentDir
+    # pause
+}
 
-setx LUA_INIT_5_3 "@D:\ZeloEngine\Src\Script\global_init.lua"
-setx LUA_PATH_5_3 @"
-D:\ZeloEngine\Src\Script\?.lua
-D:\ZeloEngine\Reference\Lua-Game-AI-Programming\script\?.lua
-D:\LuaRocks\lua\?.lua;
-D:\LuaRocks\lua\?\init.lua;
-D:\ZeloEngine\lua\?.lua;
-D:\ZeloEngine\lua\?\init.lua;
-D:\ZeloEngine\?.lua;
-D:\ZeloEngine\?\init.lua;
-D:\ZeloEngine\..\share\lua\5.3\?.lua;
-D:\ZeloEngine\..\share\lua\5.3\?\init.lua;
-.\?.lua;
-.\?\init.lua;
-C:\Users\91018\AppData\Roaming/luarocks/share/lua/5.3/?.lua;
-C:\Users\91018\AppData\Roaming/luarocks/share/lua/5.3/?/init.lua;
-D:\LuaRocks\systree/share/lua/5.3/?.lua;
-D:\LuaRocks\systree/share/lua/5.3/?/init.lua;
-"@.Replace("\n", "")
+function BuildZeloEngine {
+    Write-Output @"
+=================================================
+==== cmake config-generate zelo-engine start ====
+=================================================
+"@
+    New-Item -ItemType Directory -Force -Path build  # https://stackoverflow.com/questions/16906170/create-directory-if-it-does-not-exist
+    Set-Location build
+    cmake -DCMAKE_TOOLCHAIN_FILE=D:/vcpkg/scripts/buildsystems/vcpkg.cmake -G  "Visual Studio 16" ..
 
-setx LUA_CPATH_5_3 @"
-D:\ZeloEngine\?.dll;
-D:\ZeloEngine\..\lib\lua\5.3\?.dll;
-D:\ZeloEngine\loadall.dll;.\?.dll;
-D:\ZeloEngine\?53.dll;
-.\?53.dll;
-C:\Users\91018\AppData\Roaming/luarocks/lib/lua/5.3/?.dll;
-D:\LuaRocks\systree/lib/lua/5.3/?.dll
-"@.Replace("\n", "")
+    Write-Output @"
+===============================================
+==== visual studio build zelo-engine start ====
+===============================================
+"@
+    cmake --build . --config debug
+}
 
-# Write-Output ================================
-# Write-Output ==== cmake build zelo start ====
-# Write-Output ================================
-# mkdir build
-Set-Location build
-cmake -DCMAKE_TOOLCHAIN_FILE=D:/vcpkg/scripts/buildsystems/vcpkg.cmake -G  "Visual Studio 16" ..
+Initialize
+BuildZeloEngine
+Finialize
 
-# function buildVS
-# {
-#     param
-#     (
-#         [parameter(Mandatory=$true)]
-#         [String] $path
-#     )
-#     process
-#     {
-#         $msBuildExe = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
-
-#         Write-Host "Building $($path)" -foregroundcolor green
-#         & "$($msBuildExe)" "$($path)" /t:Build /m
-#     }
-# }
-
-# Write-Output @"
-# =======================
-# ==== msbuild start ====
-# =======================
-# "@
-# buildVS ZeloEngine.sln
-
-# @rem lib build switch, uncomment this only when you want to rebuild lib
-# rem call External/build.bat
-
-# @rem call submodule build here
-# rem call d3d12book/build.bat
-# rem call GameEngineFromScratch/Article21_DX12App/build.bat
-
-# Write-Output =======================
-# Write-Output ==== build.ps1 end ====
-# Write-Output =======================
-# Set-Location $CurrentDir
-# pause
