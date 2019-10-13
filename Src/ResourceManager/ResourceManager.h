cbbee766178860c1e28068d23be9ccd5c1167774
zoloypzuo <910181598@qq.com>
2019-10-13 16:17:17 +0800

    BIG dir refactor; move lua config to Config; move lua-config-manager to LuaModule; add ResourceManager; add Framework

---
+++ Src/ResourceManager/ResourceManager.h
@@ -0,0 +1,14 @@
+// ResourceManager.h
+// created on 2019/10/13
+// author @zoloypzuo
+
+#ifndef ZELOENGINE_RESOURCEMANAGER_H
+#define ZELOENGINE_RESOURCEMANAGER_H
+
+
+class ResourceManager {
+
+};
+
+
+#endif //ZELOENGINE_RESOURCEMANAGER_H
