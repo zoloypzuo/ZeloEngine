8d360a29a81a01d4a9228eb95ef99a8b20ee4213
zoloypzuo <910181598@qq.com>
2019-10-15 17:00:33 +0800

    阅读《Lua-Game-AI》第三章，fix #206
    Former-commit-id: cc7a5d65c516281b8cf74c4c4baebc03af4b871f

--- Reference/Lua-Game-AI-Programming/src/demo_framework/script/Animation/AnimationState.lua
+++ Reference/Lua-Game-AI-Programming/src/demo_framework/script/Animation/AnimationState.lua
@@ -1,18 +1,23 @@
+-- 动画的状态
 AnimationState = {};
 
 function AnimationState.new()
     local state = {};
 
+    -- 动画
     -- Contains the Ogre AnimationState animation.
     state.animation_ = nil;
 
+    -- 状态名
     -- The name of the animation state, this must be unique within the
     -- animation state machine.
     state.name_ = "";
 
+    -- 状态是否循环
     -- Whether the animation is looping.
     state.looping_ = false;
 
+    -- 动画播放速率
     -- How fast the animation should play.
     state.rate_ = 1;
 
