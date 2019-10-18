10df9700f7dcc5e7b33b73a0d214ad1577a314d2
zoloypzuo <910181598@qq.com>
2019-10-18 21:15:53 +0800

    rename
    Former-commit-id: 964434c0a38c97190330f6738e457f9e4140081d

--- Config/CMakeProjectConfig/ImportLibConfig_SandboxFramework.lua
+++ Config/CMakeProjectConfig/ImportLibConfig_SandboxFramework.lua
@@ -1,27 +1,28 @@
--- ImportLibConfig_sandbox.lua
+-- ImportLibConfig_SandboxFramework.lua
 -- created on 2019/10/18
 -- author @zoloypzuo
 
 -- zelo直接导入Renference/Lua-Game-AI
 -- 我们看到premake.lua的两个地方
 
-DxHeaderDir = cmake_string [[C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Include]]
-DxLibx64Dir = cmake_string [[C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Lib/x64]]
+DxHeaderDir = [[C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Include]]
+DxLibx64Dir = [[C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Lib/x64]]
 DxLibNames = {
     "d3d9",
     "dinput8",
     "dxguid",
     "d3dx9",
     "DxErr",
-    "vcruntimed.lib",
+    --"vcruntimed.lib",
     --"ucrtd.lib",
     --"libucrt.lib",
     --"libucrtd.lib"
 }
 
 local import_lib_config = ImportLibConfig()
-import_lib_config.include_dirs = {
+import_lib_config.include_dirs = map(cmake_string, {
     "D:/ZeloEngine/Reference/Lua-Game-AI-Programming/src/",
+    "D:/ZeloEngine/Reference/Lua-Game-AI-Programming/src/lua/include/",
     "D:/ZeloEngine/Reference/Lua-Game-AI-Programming/src/bullet_collision/include/",
     "D:/ZeloEngine/Reference/Lua-Game-AI-Programming/src/bullet_dynamics/include/",
     "D:/ZeloEngine/Reference/Lua-Game-AI-Programming/src/bullet_linearmath/include/",
@@ -30,7 +31,7 @@ import_lib_config.include_dirs = {
     "D:/ZeloEngine/Reference/Lua-Game-AI-Programming/src/ogre3d/include/",
     "D:/ZeloEngine/Reference/Lua-Game-AI-Programming/src/ogre3d_direct3d9/include/",
     DxHeaderDir
-}
+})
 
 import_lib_config.lib_names = {
     "bullet_collision",
@@ -66,10 +67,6 @@ import_lib_config.lib_names = {
     "dxguid",
     "d3dx9",
     "DxErr",
-    "vcruntimed.lib",
-    "ucrtd.lib",
-    "libucrt.lib",
-    "libucrtd.lib"
 }
 
 import_lib_config.lib_dirs = {
