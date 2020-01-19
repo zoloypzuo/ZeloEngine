26c13c0c6da943ef4c5a82e37be338f5d1e92b5f
zoloypzuo <910181598@qq.com>
2020-01-20 03:41:04 +0800

    Update README.md
    Former-commit-id: a923cfb14c625d1ec44676b90021ce2eb68acc92 [formerly 5c2c1e3fef39bc72a77423792dee7739622dceef]
    Former-commit-id: cf1f3b4f4ecaed7d3c2681213d8f6b02a655d45c

--- MiniProject/SandboxFramework/script/GodCamera.lua
+++
@@ -1,9 +0,0 @@
--- GodCamera.lua
--- created on 2019/10/7
--- author @zoloypzuo
-
-local GodCamera = Class(function()
-
-end)
-
-return GodCamera
--- MiniProject/SandboxFramework/src/my_sandbox/script/AgentUtilities.lua
+++
@@ -1,6 +0,0 @@
--- AgentUtilities.lua
-
-function AgentUtilities_CreateAgentRepresentation(agent, height, radius)
-    local capsule = Core.CreateCapsule(agent, height, radius)
-    Core.SetMaterial(capsule, "Ground2")
-end
