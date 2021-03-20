45f8b3686bc330620d687315337ed17015e760a6
左亦 <910181598@qq.com>
2021-03-21 00:12:54 +0800

    - (#213)

--- Config/CMakeProjectConfig/ImportLibConfig_ZeloEngine.lua
+++
@@ -1,9 +0,0 @@
--- ImportLibConfig_ZeloEngine.lua
--- created on 2019/10/18
--- author @zoloypzuo
-
-local self = ImportLibConfig()
-self.include_dirs = List {}
-self.lib_dirs = List {}
-self.lib_names = List { "Common" }
-return self
