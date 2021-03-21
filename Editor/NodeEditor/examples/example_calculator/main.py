f784c8c6a5f6c5194e09f74dd377de25e07a3af0
zoloypzuo <910181598@qq.com>
2021-03-21 12:35:39 +0800

    node editor

---
+++ Editor/NodeEditor/examples/example_calculator/main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from PyQt5.QtWidgets import *
+
+sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
+
+from examples.example_calculator.calc_window import CalculatorWindow
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    # print(QStyleFactory.keys())
+    app.setStyle('Fusion')
+
+    wnd = CalculatorWindow()
+    wnd.show()
+
+    sys.exit(app.exec_())
