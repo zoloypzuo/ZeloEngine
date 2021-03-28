a517ba38da404f463f75c997f46a210a26524313
zoloypzuo <910181598@qq.com>
2021-03-28 13:01:15 +0800

    rm shit

--- Src/Common_ARHIVED/GlobalConfig.cpp
+++
@@ -1,8 +0,0 @@
-// GlobalConfig.cpp
-// created on 2019/9/23
-// author @zoloypzuo
-
-#include "GlobalConfig.h"
-
-std::string g_engineRootDir = R"(D:\ZeloEngine\)";
-std::string g_configFileDir = R"(D:\ZeloEngine\Src\LuaConfig)";
--- Src/Common_ARHIVED/LuaConfigManager.cpp
+++
@@ -1,5 +0,0 @@
-// LuaConfigManager.cpp
-// created on 2019/9/24
-// author @zoloypzuo
-
-#include "LuaConfigManager.h"
