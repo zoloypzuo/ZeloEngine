8d5200e6cdf5ed4a8b84380a2ed36cabf70906c3
zoloypzuo <910181598@qq.com>
2021-03-29 22:40:58 +0800

    flatten src

--- build_swig_simple.bat
+++ build_swig_simple.bat
@@ -5,7 +5,7 @@ set Args=%*
 cd /d %ScriptDir%
 
 @echo on
-swig -python -c++ -Wall -o Engine/src/zelo_wrapper.cxx -outdir ./build_swig Engine/include/zelo.i
-swig -python -c++ -E Engine/include/zelo.i >build_swig/swig.log
+swig -python -c++ -Wall -o Engine/zelo_wrapper.cxx -outdir ./build_swig Engine/zelo.i
+swig -python -c++ -E Engine/zelo.i >build_swig/swig.log
 
 cd %CurrentDir%
