25e4cee156487aa36f3e6feb8bbd150527acadb6
左亦 <910181598@qq.com>
2021-04-02 11:03:17 +0800

    shield spdlog internal error

--- Engine/ZeloPreCompiledHeader.h
+++ Engine/ZeloPreCompiledHeader.h
@@ -5,7 +5,12 @@
 #ifndef ZELOENGINE_ZELOPRECOMPILEDHEADER_H
 #define ZELOENGINE_ZELOPRECOMPILEDHEADER_H
 
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
 #include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#pragma warning(pop)
+
 #include "G.h"
 #include "Mathf.h"
 
