9d42451419de0396ae350a8d9347052c43ee6844
zoloypzuo <910181598@qq.com>
2021-04-04 00:35:46 +0800

    In Game Editor

---
+++ Engine/InGameEditor/EditorLayer.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "Zelo.h"
+#include "SceneHierarchyPanel.h"
+
+//#include "Framework/Renderer/EditorCamera.h"
+
+namespace Zelo {
+
+//class EditorLayer : public Layer {
+//public:
+//    EditorLayer();
+//
+//    virtual ~EditorLayer() = default;
+//
+//    virtual void OnAttach() override;
+//
+//    virtual void OnDetach() override;
+//
+//    void OnUpdate(Timestep ts) override;
+//
+//    virtual void OnImGuiRender() override;
+//
+//    void OnEvent(Event &e) override;
+//
+//private:
+//    bool OnKeyPressed(KeyPressedEvent &e);
+//
+//    void NewScene();
+//
+//    void OpenScene();
+//
+//    void SaveSceneAs();
+//
+//private:
+//    Zelo::OrthographicCameraController m_CameraController;
+//
+//    // Temp
+//    std::shared_ptr <VertexArray> m_SquareVA;
+//    std::shared_ptr <Shader> m_FlatColorShader;
+//    std::shared_ptr <Framebuffer> m_Framebuffer;
+//
+//    std::shared_ptr <Scene> m_ActiveScene;
+//    Entity m_SquareEntity;
+//    Entity m_CameraEntity;
+//    Entity m_SecondCamera;
+//
+//    bool m_PrimaryCamera = true;
+//
+//    EditorCamera m_EditorCamera;
+//
+//    std::shared_ptr <Texture2D> m_CheckerboardTexture;
+//
+//    bool m_ViewportFocused = false, m_ViewportHovered = false;
+//    glm::vec2 m_ViewportSize = {0.0f, 0.0f};
+//
+//    glm::vec4 m_SquareColor = {0.2f, 0.3f, 0.8f, 1.0f};
+//
+//    int m_GizmoType = -1;
+//
+//    // Panels
+//    SceneHierarchyPanel m_SceneHierarchyPanel;
+//};
+
+}
---
+++ Engine/InGameEditor/InGameEditor.cpp
@@ -0,0 +1,27 @@
+#include <Zelo.h>
+//#include <Zelo/Core/EntryPoint.h>
+
+//#include "EditorLayer.h"
+
+namespace Zelo {
+
+//class InGameEditor : public Application {
+//public:
+//    InGameEditor()
+//            : Application("InGameEditor") {
+//        PushLayer(new EditorLayer());
+//    }
+//
+//    ~InGameEditor() {
+//    }
+//};
+//
+//Application *CreateApplication() {
+//    return new InGameEditor();
+//}
+
+}
+
+int main(){
+    return 0;
+}
