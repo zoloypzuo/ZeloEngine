5d2de50b67296f339e34a7c546bb65ed6fa60494
zoloypzuo <910181598@qq.com>
2021-04-03 12:00:38 +0800

    debug break

--- Engine/ZeloPreCompiledHeader.h
+++ Engine/ZeloPreCompiledHeader.h
@@ -23,5 +23,20 @@
 
 #endif
 
+// debug break
+#ifdef ZELO_DEBUG
+  #if defined(ZELO_PLATFORM_WINDOWS)
+    #define ZELO_DEBUGBREAK() __debugbreak()
+  #elif defined(ZELO_PLATFORM_LINUX)
+    #include <signal.h>
+    #define ZELO_DEBUGBREAK() raise(SIGTRAP)
+  #else
+    #error "Platform doesn't support debugbreak yet!"
+  #endif
+  #define ZELO_ENABLE_ASSERTS
+#else
+  #define ZELO_DEBUGBREAK()
+#endif
+
 
 #endif //ZELOENGINE_ZELOPRECOMPILEDHEADER_H
