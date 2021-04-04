9538ebfc09480c078f163a418985bec4813359fc
zoloypzuo <910181598@qq.com>
2021-04-04 15:25:26 +0800

    fix build mistake

--- Tools/build_swig.bat
+++ Tools/build_swig.bat
@@ -9,11 +9,10 @@ set Args=%*
 
 cd /d %EngineDir%
 @echo on
+mkdir build_swig
 swig -python -c++ -Wall -o Engine/zelo_wrapper.cxx -outdir ./build_swig Engine/zelo.i
 swig -python -c++ -E Engine/zelo.i >build_swig/swig.log
 
-pause
-mkdir build_swig
 cd build_swig
 cmake -DCMAKE_GENERATOR_PLATFORM=win32 -DBuildSwig=ON -G  "Visual Studio 15" ..
 cmake --build . --config release --target zelo_py
