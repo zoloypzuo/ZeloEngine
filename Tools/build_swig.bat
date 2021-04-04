4cd878bebc15c04015b51891402cbfa065fa798e
zoloypzuo <910181598@qq.com>
2021-04-04 14:09:31 +0800

    ahk

--- Tools/build_swig.bat
+++ Tools/build_swig.bat
@@ -1,12 +1,18 @@
+@rem build_swig.bat
+@rem created on 2021/4/4
+@rem author @zoloypzuo
 @echo off
 set CurrentDir=%cd%
 set ScriptDir=%~dp0
+set EngineDir=%ScriptDir%\..
 set Args=%*
-cd /d %ScriptDir%
 
+cd /d %EngineDir%
 @echo on
-call build_swig_simple.bat
+swig -python -c++ -Wall -o Engine/zelo_wrapper.cxx -outdir ./build_swig Engine/zelo.i
+swig -python -c++ -E Engine/zelo.i >build_swig/swig.log
 
+pause
 mkdir build_swig
 cd build_swig
 cmake -DCMAKE_GENERATOR_PLATFORM=win32 -DBuildSwig=ON -G  "Visual Studio 15" ..
@@ -17,7 +23,6 @@ cd %ScriptDir%
 copy build_swig\zelo.py Script\scriptlibs\zelo.py
 xcopy build_swig\Engine\Release\*.dll Script\scriptlibs\ /y
 move Script\scriptlibs\zelo_py.dll Script\scriptlibs\_zelo.pyd
-
 
 cd %CurrentDir%
 pause
