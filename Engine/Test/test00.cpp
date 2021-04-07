c26569593070c8a2226ebc9b7e142b2c1868e5ce
左亦 <910181598@qq.com>
2021-04-07 13:01:55 +0800

    add test

---
+++ Engine/Test/test00.cpp
@@ -0,0 +1,22 @@
+// test00.cpp
+// created on 2021/4/7
+// author @zoloypzuo
+#include "ZeloPreCompiledHeader.h"
+#include "test00.h"
+#include "gtest/gtest.h"
+//#include "exp1.h"
+
+namespace {
+TEST(Exp1Test, add) {
+    double res;
+//res = add_numbers(1.0, 2.0);
+//ASSERT_NEAR(res, 3.0, 1.0e-11);
+}
+
+TEST(Exp1Test, subtract) {
+//double res;
+//res = subtract_numbers(1.0, 2.0);
+//ASSERT_NEAR(res, -1.0, 1.0e-11);
+}
+}
+
---
+++ Engine/Test/test00.h
@@ -0,0 +1,14 @@
+// test00.h
+// created on 2021/4/7
+// author @zoloypzuo
+
+#pragma once
+
+#include "ZeloPrerequisites.h"
+
+
+class test00 {
+
+};
+
+
