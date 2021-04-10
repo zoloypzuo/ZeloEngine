533d596ee9a46232f74cd5b58480a63f64235dc2
zoloypzuo <910181598@qq.com>
2021-04-10 17:00:41 +0800

    RelWithDebInfo

--- Tools/build_swig.bat
+++ Tools/build_swig.bat
@@ -15,12 +15,12 @@ swig -python -c++ -E Engine/zelo.i >build_swig/swig.log
 
 cd build_swig
 cmake -DCMAKE_GENERATOR_PLATFORM=win32 -DBuildSwig=ON -G  "Visual Studio 16" ..
-cmake --build . --config release --target zelo_py
+cmake --build . --config RelWithDebInfo --target zelo_py
 
 cd /d %EngineDir%
 
 copy build_swig\zelo.py Script\scriptlibs\zelo.py
-xcopy build_swig\Engine\Release\*.dll Script\scriptlibs\ /y
+xcopy build_swig\Engine\RelWithDebInfo\* Script\scriptlibs\ /y
 move Script\scriptlibs\zelo_py.dll Script\scriptlibs\_zelo.pyd
 
 cd %CurrentDir%
