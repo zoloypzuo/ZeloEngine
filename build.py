5166b223fca5ce5d47d5ab24884d1ec2d4741efc
左亦 <910181598@qq.com>
2021-04-25 17:57:04 +0800

    vld.ini

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 import os
-
+import shutil
 import sys
 
 engine_dir = sys.argv[1]
 exe_dir = sys.argv[2]
+
+# create boot.ini
 boot_ini_path = os.path.join(exe_dir, "boot.ini")
 content_template = """
 [boot]
@@ -14,4 +16,8 @@ print "write to =>", boot_ini_path
 with open(boot_ini_path, "w") as fp:
     fp.write(content)
 
-# TODO copy vld.ini
+# copy vld.ini
+vld_ini_src_path = os.path.join(engine_dir, "Config/vld.ini")
+vld_ini_dest_path = os.path.join(exe_dir, "vld.ini")
+print "copy to %s => %s" % (vld_ini_src_path, vld_ini_dest_path)
+shutil.copy(vld_ini_src_path, vld_ini_dest_path)
