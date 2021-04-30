87c765ea03c4806facb9c9d5ec00075a878d2354
左亦 <910181598@qq.com>
2021-04-30 15:55:09 +0800

    Update forward-directional.lua

--- Shader/forward-directional.lua
+++ Shader/forward-directional.lua
@@ -13,9 +13,17 @@ out vec2 texCoord0;
 out vec3 worldPos0;
 out mat3 tbnMatrix;
 
+// shadow
+out vec3 FragPos;
+out vec4 FragPosLightSpace;
+
+
 uniform mat4 View;
 uniform mat4 Proj;
 uniform mat4 World;
+
+// shadow
+uniform mat4 lightSpaceMatrix;
 
 void main()
 {
@@ -29,15 +37,24 @@ void main()
 
   vec3 biTangent = cross(t, n);
   tbnMatrix = mat3(t, biTangent, n);
+
+  // shadow
+  FragPos = vec3(World * vec4(position, 1.0));
+  FragPosLightSpace = lightSpaceMatrix * vec4(FragPos, 1.0);
 }
 ]]
 
 local fragment_shader = [[
 #version 330
-
+// TODO copy paste fs code
+// TODO change C++ code with shader
 in vec2 texCoord0;
 in vec3 worldPos0;
 in mat3 tbnMatrix;
+
+// shadow
+in vec3 FragPos;  // TODO remove it
+in vec4 FragPosLightSpace;
 
 out vec4 fragColor;
 
@@ -63,41 +80,50 @@ uniform sampler2D diffuseMap;
 uniform sampler2D normalMap;
 uniform sampler2D specularMap;
 
-vec4 calculateLight(BaseLight base, vec3 direction, vec3 normal)
+// shadow
+uniform sampler2D shadowMap;
+uniform vec3 lightPos;
+
+float ShadowCalculation(vec4 fragPosLightSpace)
 {
-  float diffuseFactor = dot(normal, -direction);
+    // perform perspective divide
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    // transform to [0,1] range
+    projCoords = projCoords * 0.5 + 0.5;
+    // get closest depth value from light's perspective (using [0,1] range fragPosLight as coords)
+    float closestDepth = texture(shadowMap, projCoords.xy).r;
+    // get depth of current fragment from light's perspective
+    float currentDepth = projCoords.z;
+    // check whether current frag pos is in shadow
+    float shadow = currentDepth > closestDepth  ? 1.0 : 0.0;
 
-  vec4 diffuseColor = vec4(0.0f, 0.0f, 0.0f, 0.0f);
-  vec4 specularColor = vec4(0.0f, 0.0f, 0.0f, 0.0f);
-
-  if (diffuseFactor > 0.0f)
-  {
-    diffuseColor = vec4(base.color, 1.0f) * base.intensity * diffuseFactor;
-
-    vec3 directionToEye = normalize(eyePos - worldPos0);
-    vec3 reflectDirection = normalize(reflect(direction, normal));
-
-    float specularFactor = dot(directionToEye, reflectDirection);
-    specularFactor = pow(specularFactor, specularPower);
-
-    if (specularFactor > 0.0f)
-    {
-      specularColor = vec4(base.color, 1.0f) * (specularIntensity * specularFactor);
-    }
-  }
-
-  return diffuseColor + specularColor;
+    return shadow;
 }
 
-vec4 calculateDirectionalLight(DirectionalLight directionalLight, vec3 normal)
-{
-  return calculateLight(directionalLight.base, directionalLight.direction, normal);
-}
 
 void main()
 {
   vec3 normal = normalize(tbnMatrix * (255.0/128.0 * texture(normalMap, texCoord0).xyz - 1));
-  fragColor = texture(diffuseMap, texCoord0) * calculateDirectionalLight(directionalLight, normal);
+  vec3 color = texture(diffuseMap, texCoord0).rgb;
+  vec3 lightColor = vec3(0.3);
+  // ambient
+  vec3 ambient = 0.3 * color;
+  // diffuse
+  vec3 lightDir = normalize(lightPos - FragPos);
+  float diff = max(dot(lightDir, normal), 0.0);
+  vec3 diffuse = diff * lightColor;
+  // specular
+  vec3 viewDir = normalize(eyePos - FragPos);
+  vec3 reflectDir = reflect(-lightDir, normal);
+  float spec = 0.0;
+  vec3 halfwayDir = normalize(lightDir + viewDir);
+  spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+  vec3 specular = spec * lightColor;
+  // calculate shadow
+  float shadow = ShadowCalculation(FragPosLightSpace);
+  vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;
+
+  fragColor = vec4(lighting, 1.0);
 }
 ]]
 
