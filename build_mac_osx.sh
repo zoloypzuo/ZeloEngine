cc71793f66657f3b89cfe122e627d14122aea7cf
左亦 <910181598@qq.com>
2021-06-20 15:19:12 +0800

    Update build_mac_osx.sh

--- build_mac_osx.sh
+++ build_mac_osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # copy lua header to include, fix sol compile error
-# cp /home/zuoyiping01/ZeloEngine/Engine/Core/Lua/include/* /usr/local/include/
+cp /Users/zoloypzuo/Documents/ZeloEngine/Engine/Core/Lua/include/* /usr/local/include/
 
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/Users/zoloypzuo/Documents/vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build build
