56bd98dae1e7e112d836fe5e9244f78363fdc6a3
zoloypzuo <910181598@qq.com>
2021-07-27 00:38:43 +0800

    Actor

---
+++ Engine/Core/ECS/Actor.cpp
@@ -0,0 +1,5 @@
+// Actor.cpp.cc
+// created on 2021/7/27
+// author @zoloypzuo
+#include "ZeloPreCompiledHeader.h"
+#include "Actor.h"
---
+++ Engine/Core/ECS/Actor.h
@@ -0,0 +1,15 @@
+// Actor.h
+// created on 2021/7/27
+// author @zoloypzuo
+#pragma once
+
+#include "ZeloPrerequisites.h"
+
+
+namespace Zelo::Core::ECS {
+class Actor {
+
+};
+}
+
+
