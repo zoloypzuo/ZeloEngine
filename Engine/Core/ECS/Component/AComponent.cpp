3fb987ad91749afcffb04b186852f7a45391cba6
zoloypzuo <910181598@qq.com>
2021-07-27 11:28:13 +0800

    cleanup

--- Engine/Core/ECS/Component/AComponent.cpp
+++
@@ -1,5 +0,0 @@
-// AComponent.cpp.cc
-// created on 2021/7/27
-// author @zoloypzuo
-#include "ZeloPreCompiledHeader.h"
-#include "AComponent.h"
--- Engine/Core/ECS/Component/Behaviour.cpp
+++
@@ -1,5 +0,0 @@
-// Behaviour.cpp.cc
-// created on 2021/7/26
-// author @zoloypzuo
-#include "ZeloPreCompiledHeader.h"
-#include "Behaviour.h"
