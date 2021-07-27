e4e24497ea9f036eb8430b7096aee00a4a4be1dc
zoloypzuo <910181598@qq.com>
2021-07-27 15:46:41 +0800

    Update AComponent.h

--- Engine/Core/ECS/Components/AComponent.h
+++ Engine/Core/ECS/Components/AComponent.h
@@ -1,6 +1,4 @@
 #pragma once
-
-//#include "Core/API/IInspectorItem.h"
 #include "ZeloPrerequisites.h"
 
 namespace Zelo::Core::ECS { class Actor; }
@@ -8,7 +6,7 @@ namespace Zelo::Core::ECS { class Actor; }
 namespace Zelo::Core::ECS::Components {
 class CPhysicalObject;
 
-class AComponent /*: public API::IInspectorItem*/ {
+class AComponent{
 public:
 
     explicit AComponent(ECS::Actor &owner);
@@ -30,23 +28,6 @@ public:
     virtual void OnFixedUpdate(float deltaTime) {}
 
     virtual void OnLateUpdate(float deltaTime) {}
-
-//    virtual void OnCollisionEnter(Components::CPhysicalObject &otherObject) {}
-//
-//
-//    virtual void OnCollisionStay(Components::CPhysicalObject &otherObject) {}
-//
-//
-//    virtual void OnCollisionExit(Components::CPhysicalObject &otherObject) {}
-//
-//
-//    virtual void OnTriggerEnter(Components::CPhysicalObject &otherObject) {}
-//
-//
-//    virtual void OnTriggerStay(Components::CPhysicalObject &otherObject) {}
-//
-//
-//    virtual void OnTriggerExit(Components::CPhysicalObject &otherObject) {}
 
     virtual std::string GetName() = 0;
 
