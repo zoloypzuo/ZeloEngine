3f86147b3c7abc48ad5c4855a760172a41842c90
左亦 <910181598@qq.com>
2021-07-30 19:45:54 +0800

    add Time

---
+++ Engine/Core/OS/Time.cpp
@@ -0,0 +1,5 @@
+// Time.cpp
+// created on 2021/7/30
+// author @zoloypzuo
+#include "ZeloPreCompiledHeader.h"
+#include "Time.h"
---
+++ Engine/Core/OS/Time.h
@@ -0,0 +1,13 @@
+// Time.h
+// created on 2021/7/30
+// author @zoloypzuo
+#pragma once
+
+#include "ZeloPrerequisites.h"
+
+
+class Time {
+
+};
+
+
