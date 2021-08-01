c186b45214a78d308f744779f9b66b5c54018cd4
zoloypzuo <910181598@qq.com>
2021-08-01 11:51:25 +0800

    RenderSystem

---
+++ Engine/Core/RHI/RenderSystem.cpp
@@ -0,0 +1,31 @@
+// RenderSystem.cpp
+// created on 2021/8/1
+// author @zoloypzuo
+#include "ZeloPreCompiledHeader.h"
+#include "RenderSystem.h"
+
+using namespace Zelo::Core::RHI;
+
+RenderSystem::~RenderSystem() = default;
+
+void RenderSystem::initialize() {
+
+}
+
+void RenderSystem::update() {
+
+}
+
+void RenderSystem::finalize() {
+
+}
+
+const RenderSystem::FrameInfo &RenderSystem::GetFrameInfo() const {
+    return m_frameInfo;
+}
+
+void RenderSystem::ClearFrameInfo() {
+    m_frameInfo.batchCount = 0;
+    m_frameInfo.instanceCount = 0;
+    m_frameInfo.polyCount = 0;
+}
---
+++ Engine/Core/RHI/RenderSystem.h
@@ -0,0 +1,39 @@
+// RenderSystem.h
+// created on 2021/8/1
+// author @zoloypzuo
+#pragma once
+
+#include "ZeloPrerequisites.h"
+#include "ZeloSingleton.h"
+
+namespace Zelo::Core::RHI {
+class RenderSystem :
+        public Singleton<RenderSystem>,
+        IRuntimeModule {
+public:
+    struct FrameInfo {
+        uint64_t batchCount = 0;
+        uint64_t instanceCount = 0;
+        uint64_t polyCount = 0;
+    };
+
+public:
+    ~RenderSystem() override;
+
+    void initialize() override;
+
+    void update() override;
+
+    void finalize() override;
+
+public:
+    const FrameInfo &GetFrameInfo() const;
+
+    void ClearFrameInfo();
+
+private:
+    FrameInfo m_frameInfo;
+};
+}
+
+
