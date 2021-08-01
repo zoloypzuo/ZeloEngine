1888295cbe9b1afae74e741f03e99b51060c412b
zoloypzuo <910181598@qq.com>
2021-08-01 13:08:27 +0800

    RC

--- Engine/Core/RHI/Const/ERenderingCapability.h
+++ Engine/Core/RHI/Const/ERenderingCapability.h
@@ -64,4 +64,45 @@ enum class EOperation
     DECREMENT_WRAP	= 0x8508,
     INVERT			= 0x150A
 };
+
+enum class EPixelDataFormat
+{
+    COLOR_INDEX					= 0x1900,
+    STENCIL_INDEX				= 0x1901,
+    DEPTH_COMPONENT				= 0x1902,
+    RED							= 0x1903,
+    GREEN						= 0x1904,
+    BLUE						= 0x1905,
+    ALPHA						= 0x1906,
+    RGB							= 0x1907,
+    BGR							= 0x80E0,
+    RGBA						= 0x1908,
+    BGRA						= 0x80E1,
+    LUMINANCE					= 0x1909,
+    LUMINANCE_ALPHA				= 0x190A,
+};
+
+enum class EPixelDataType
+{
+    BYTE						= 0x1400,
+    UNSIGNED_BYTE				= 0x1401,
+    BITMAP						= 0x1A00,
+    SHORT						= 0x1402,
+    UNSIGNED_SHORT				= 0x1403,
+    INT							= 0x1404,
+    UNSIGNED_INT				= 0x1405,
+    FLOAT						= 0x1406,
+    UNSIGNED_BYTE_3_3_2			= 0x8032,
+    UNSIGNED_BYTE_2_3_3_REV		= 0x8362,
+    UNSIGNED_SHORT_5_6_5		= 0x8363,
+    UNSIGNED_SHORT_5_6_5_REV	= 0x8364,
+    UNSIGNED_SHORT_4_4_4_4		= 0x8033,
+    UNSIGNED_SHORT_4_4_4_4_REV	= 0x8365,
+    UNSIGNED_SHORT_5_5_5_1		= 0x8034,
+    UNSIGNED_SHORT_1_5_5_5_REV	= 0x8366,
+    UNSIGNED_INT_8_8_8_8		= 0x8035,
+    UNSIGNED_INT_8_8_8_8_REV	= 0x8367,
+    UNSIGNED_INT_10_10_10_2		= 0x8036,
+    UNSIGNED_INT_2_10_10_10_REV	= 0x8368
+};
 }
--- Engine/Core/RHI/RenderCommand.h
+++ Engine/Core/RHI/RenderCommand.h
@@ -34,7 +34,58 @@ public:
 
     virtual bool getCapabilityEnabled(ERenderingCapability capability) = 0;
 
-    virtual void 
+    virtual void setStencilAlgorithm(EComparaisonAlgorithm algorithm, int32_t reference, uint32_t mask) = 0;
+
+    virtual void setDepthAlgorithm(EComparaisonAlgorithm algorithm) = 0;
+
+    virtual void setStencilMask(uint32_t mask) = 0;
+
+    virtual void setStencilOperations(EOperation stencilFail, EOperation depthFail, EOperation bothPass) = 0;
+
+    virtual void setCullFace(ECullFace cullFace) = 0;
+
+    virtual void setDepthWriting(bool enable) = 0;
+
+    virtual void setColorWriting(bool enableRed, bool enableGreen, bool enableBlue, bool enableAlpha) = 0;
+
+    virtual void setColorWriting(bool enable) = 0;
+
+    virtual void setViewPort(uint32_t x, uint32_t y, uint32_t width, uint32_t height) = 0;
+
+    virtual void readPixels(uint32_t x, uint32_t y, uint32_t width, uint32_t height,
+                            EPixelDataFormat format, EPixelDataType type,
+                            void *data) = 0;
+
+    virtual bool GetBool(uint32_t/*GLenum*/ parameter) = 0;
+
+    virtual bool GetBool(uint32_t/*GLenum*/ parameter, uint32_t index) = 0;
+
+    virtual int GetInt(uint32_t/*GLenum*/ parameter) = 0;
+
+    virtual int GetInt(uint32_t/*GLenum*/ parameter, uint32_t index) = 0;
+
+    virtual float GetFloat(uint32_t/*GLenum*/ parameter) = 0;
+
+    virtual float GetFloat(uint32_t/*GLenum*/ parameter, uint32_t index) = 0;
+
+    virtual double GetDouble(uint32_t/*GLenum*/ parameter) = 0;
+
+    virtual double GetDouble(uint32_t/*GLenum*/ parameter, uint32_t index) = 0;
+
+    virtual int64_t GetInt64(uint32_t/*GLenum*/ parameter) = 0;
+
+    virtual int64_t GetInt64(uint32_t/*GLenum*/ parameter, uint32_t index) = 0;
+
+    virtual std::string GetString(uint32_t/*GLenum*/ parameter) = 0;
+
+    virtual std::string GetString(uint32_t/*GLenum*/ parameter, uint32_t index) = 0;
+
+//    virtual void Draw(Resources::IMesh& mesh, EPrimitiveMode primitiveMode = EPrimitiveMode::TRIANGLES, uint32_t instances = 1)=0;
+    virtual uint8_t FetchGLState() = 0;
+
+    virtual void ApplyStateMask(uint8_t mask) = 0;
+
+    virtual void SetState(uint8_t state) = 0;
 };
 }
 
