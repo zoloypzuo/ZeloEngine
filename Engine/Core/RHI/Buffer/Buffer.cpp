00edae0b39d4b9eb6c3d4ebeaac0a9b5ce4fecf0
zoloypzuo <910181598@qq.com>
2021-08-01 13:57:29 +0800

    cleanup

--- Engine/Core/RHI/Buffer/Buffer.cpp
+++
@@ -1,5 +0,0 @@
-// Buffer.cpp
-// created on 2021/6/6
-// author @zoloypzuo
-#include "ZeloPreCompiledHeader.h"
-#include "Buffer.h"
--- Engine/Core/RHI/Resource/Shader.cpp
+++
@@ -1,5 +0,0 @@
-// Shader.cpp
-// created on 2021/6/2
-// author @zoloypzuo
-#include "ZeloPreCompiledHeader.h"
-#include "Shader.h"
