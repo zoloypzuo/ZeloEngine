0fe1cecee2cac8e32fa68664f27d2da8f759462f
左亦 <910181598@qq.com>
2021-08-13 17:20:18 +0800

    Update monkey.lua

--- Script/Lua/prefabs/monkey.lua
+++ Script/Lua/prefabs/monkey.lua
@@ -2,10 +2,7 @@
 -- created on 2021/8/9
 -- author @zoloypzuo
 local assets = {
-    mesh     = Asset("MESH", "monkey3.obj");
-    diffuse  = Asset("TEX", "bricks2.jpg");
-    normal   = Asset("TEX", "bricks2_normal.jpg");
-    specular = Asset("TEX", "bricks2_specular.png");
+    mesh = Asset("MESH", "monkey3.obj");
 }
 
 local function fn()
