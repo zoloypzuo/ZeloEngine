495c06899d74fb8a1cca2b123ab8d82382c5f17e
zoloypzuo <910181598@qq.com>
2021-08-14 09:39:33 +0800

    vec3

--- Engine/Core/LuaScript/LuaBind/Core/ECS/LuaBind_Entity.cpp
+++ Engine/Core/LuaScript/LuaBind/Core/ECS/LuaBind_Entity.cpp
@@ -149,7 +149,13 @@ sol::constructors<
         glm::vec3(float, float, float)>(),
 "x", &glm::vec3::x,
 "y", &glm::vec3::y,
-"z", &glm::vec3::z
+"z", &glm::vec3::z,
+sol::meta_function::multiplication, sol::overload(
+    [](const glm::vec3& v1, const glm::vec3& v2) -> glm::vec3 { return v1*v2; },
+    [](const glm::vec3& v1, float f) -> glm::vec3 { return v1*f; },
+    [](float f, const glm::vec3& v1) -> glm::vec3 { return f*v1; }
+),
+"Dummpy", []{}
 );
 // @formatter:on
 
