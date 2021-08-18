1e2adc62cee34ed8dd59aa72797eba46da3d65af
zoloypzuo <910181598@qq.com>
2021-08-19 01:11:56 +0800

    Update resource_config.lua

--- Config/resource_config.lua
+++ Config/resource_config.lua
@@ -1,6 +1,7 @@
 -- resource_config.lua
 -- created on 2021/5/5
 -- author @zoloypzuo
+-- resource location root
 return {
     "D:/ZeloEngineResource/EngineAssets";
     "D:/ZeloEngineResource/GameAIAssets";
