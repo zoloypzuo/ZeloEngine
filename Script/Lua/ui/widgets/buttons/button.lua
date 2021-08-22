f7c7325df7e7216c257d8e3d74c9703d3e75dc5f
zoloypzuo <910181598@qq.com>
2021-08-22 12:03:39 +0800

    Update button.lua

--- Script/Lua/ui/widgets/buttons/button.lua
+++ Script/Lua/ui/widgets/buttons/button.lua
@@ -2,6 +2,7 @@
 -- created on 2021/8/22
 -- author @zoloypzuo
 require("framework.events")
+require("framework.vector2")
 local widget = require("ui.widget")
 
 local AButton = Class(widget.AWidget, function(self)
@@ -16,10 +17,49 @@ function AButton:_OnClick()
     self.on_click:HandleEvent("on_click")
 end
 
-local Button = Class(function(self)
-    self.label = ""
-    self.size = {}
-    self.
+local Button = Class(AButton, function(self, label, size, disabled)
+    self.label = label or ""
+    self.size = size or Vector2()
+    self.disabled = disabled or false
+
+    self.idleBackgroundColor = nil;
+    self.hoveredBackgroundColor = nil;
+    self.clickedBackgroundColor = nil;
+
+    self.textColor = nil;
+
+    -- TODO
+    -- auto &style = ImGui::GetStyle();
+    --    idleBackgroundColor = Internal::Converter::ToColor(style.Colors[ImGuiCol_Button]);
+    --    hoveredBackgroundColor = Internal::Converter::ToColor(style.Colors[ImGuiCol_ButtonHovered]);
+    --    clickedBackgroundColor = Internal::Converter::ToColor(style.Colors[ImGuiCol_ButtonActive]);
+    --    textColor = Internal::Converter::ToColor(style.Colors[ImGuiCol_Text]);
 end)
+
+function Button:_UpdateImpl()
+    --     auto &style = ImGui::GetStyle();
+    --
+    --    auto defaultIdleColor = style.Colors[ImGuiCol_Button];
+    --    auto defaultHoveredColor = style.Colors[ImGuiCol_ButtonHovered];
+    --    auto defaultClickedColor = style.Colors[ImGuiCol_ButtonActive];
+    --    auto defaultTextColor = style.Colors[ImGuiCol_Text];
+    --
+    --    style.Colors[ImGuiCol_Button] = OvUI::Internal::Converter::ToImVec4(idleBackgroundColor);
+    --    style.Colors[ImGuiCol_ButtonHovered] = OvUI::Internal::Converter::ToImVec4(hoveredBackgroundColor);
+    --    style.Colors[ImGuiCol_ButtonActive] = OvUI::Internal::Converter::ToImVec4(clickedBackgroundColor);
+    --    style.Colors[ImGuiCol_Text] = OvUI::Internal::Converter::ToImVec4(textColor);
+    --
+    --    if (ImGui::ButtonEx((label + m_widgetID).c_str(), Internal::Converter::ToImVec2(size),
+    --                        disabled ? ImGuiButtonFlags_Disabled : 0))
+    --        ClickedEvent.Invoke();
+    --
+    --    style.Colors[ImGuiCol_Button] = defaultIdleColor;
+    --    style.Colors[ImGuiCol_ButtonHovered] = defaultHoveredColor;
+    --    style.Colors[ImGuiCol_ButtonActive] = defaultClickedColor;
+    --    style.Colors[ImGuiCol_Text] = defaultTextColor;
+    if ImGui.Button(self.label, self.size.x, self.size.y) then
+        self:_OnClick()
+    end
+end
 
 return { AButton = AButton; Button = Button; }
