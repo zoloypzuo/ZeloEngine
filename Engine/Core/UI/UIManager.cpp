b887a8e64374f4cd4afeb6af21d6aa13195e038a
zoloypzuo <910181598@qq.com>
2021-08-22 10:37:17 +0800

    m_enableDocking

--- Engine/Core/UI/UIManager.cpp
+++ Engine/Core/UI/UIManager.cpp
@@ -60,6 +60,29 @@ void UIManager::update() {
     ImGui_ImplSDL2_NewFrame();
     ImGui::NewFrame();
     // Frame logic here...
+    if (m_enableDocking) {
+        ImGuiViewport *viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->Pos);
+        ImGui::SetNextWindowSize(viewport->Size);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
+        ImGui::Begin("##dockspace", nullptr,
+                     ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
+                     ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_MenuBar |
+                     ImGuiWindowFlags_NoDocking);
+        ImGuiID dockspace_id = ImGui::GetID("MyDockspace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
+        ImGui::SetWindowPos({0.f, 0.f});
+        ImVec2 displaySize = ImGui::GetIO().DisplaySize;
+        ImGui::SetWindowSize({(float) displaySize.x, (float) displaySize.y});
+        ImGui::End();
+
+        ImGui::PopStyleVar(3);
+    }
+    // Script update logic here
 }
 
 void UIManager::draw() {
