bea22a0bb33746733fef3f5b89b06db955e3c0ae
zoloypzuo <910181598@qq.com>
2021-08-22 21:47:53 +0800

    BitOp.cpp InputText

--- Engine/Core/LuaScript/LuaBind/LuaBind.cpp
+++ Engine/Core/LuaScript/LuaBind/LuaBind.cpp
@@ -13,10 +13,14 @@ void LuaBind_UI(sol::state &luaState);
 
 void LuaBind_ImGui(sol::state &luaState);
 
+LUALIB_API int luaopen_bit(lua_State *L);
+
 void LuaBind_Main(sol::state &luaState) {
     LuaBind_Global(luaState);
     LuaBind_Entity(luaState);
     LuaBind_Game(luaState);
     LuaBind_UI(luaState);
     LuaBind_ImGui(luaState);
+
+    luaState.require("bit", luaopen_bit);
 }
--- Script/Lua/ui/widgets/input_text.lua
+++ Script/Lua/ui/widgets/input_text.lua
@@ -14,22 +14,42 @@ local InputText = Class(AWidget, function(self, parent, content, label)
 end)
 
 function InputText:_UpdateImpl()
-    -- std::string previousContent = content;
-    --
-    --content.resize(256,);
-    --bool enterPressed = ImGui::InputText((label + m_widgetID).c_str(), &content[0], 256,
-    --                                        ImGuiInputTextFlags_EnterReturnsTrue |
-    --                                        (selectAllOnClick ? ImGuiInputTextFlags_AutoSelectAll : 0));
-    --content = content.c_str();
-    --
     --if (content != previousContent) {
     --    ContentChangedEvent.Invoke(content);
     --    this->NotifyChange();
     --}
-    --
-    --if (enterPressed)
-    --    EnterPressedEvent.Invoke(content);
-    local text, selected = ImGui.InputText(self.label, "?", 256)
+    local flag = 0
+    if self.selectAllOnClick then
+        flag = bit.bor(ImGuiInputTextFlags.EnterReturnsTrue, ImGuiInputTextFlags.AutoSelectAll)
+    else
+        flag = ImGuiInputTextFlags.EnterReturnsTrue
+    end
+    local text, selected = ImGui.InputText(self.label, self.content, 256, flag)
+
+    if text ~= self.content then
+        self.content = text
+        self:_OnContentChanged(text)
+    end
+
+    if selected then
+        self:_OnEnterPressed()
+    end
+end
+
+function InputText:AddOnContentChangedHandler(fn)
+    self.ContentChangedEvent:AddEventHandler("on_content_changed", fn)
+end
+
+function InputText:_OnContentChanged(content)
+    self.ContentChangedEvent:HandleEvent("on_content_changed", content)
+end
+
+function InputText:AddOnEnterPressedHandler(fn)
+    self.ContentChangedEvent:AddEventHandler("on_enter_pressed", fn)
+end
+
+function InputText:_OnEnterPressed()
+    self.ContentChangedEvent:HandleEvent("on_enter_pressed")
 end
 
 return InputText
