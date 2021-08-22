1fe8300194ade53f146eea80bbeed191fcaccf95
zoloypzuo <910181598@qq.com>
2021-08-22 18:36:52 +0800

    Update project_hub_panel.lua

--- Script/Lua/panels/project_hub_panel.lua
+++ Script/Lua/panels/project_hub_panel.lua
@@ -5,15 +5,121 @@ local panel = require("ui.panel")
 local button = require("ui.widgets.button")
 local input_text = require("ui.widgets.input_text")
 local APanel = panel.APanel
+local Button = button.Button
+local InputText = input_text.InputText
 
 local ProjectHubPanel = Class(APanel, function(self)
     APanel._ctor(self)
-    local openProjectButton = self:CreateWidget(button.Button, "Open Project")
-    openProjectButton:AddOnClickHandler(function()
-        print("button clicked")
-    end)
-    local pathField = self:CreateWidget(input_text.InputText, "?");
+
+    -- PanelWindow
+    --SetSize({ 1000, 580 });
+    --SetPosition({ 0.f, 0.f });
+
+    self:Header()
+
+    --for (uint8_t i = 0; i < 4; ++i)
+    --	CreateWidget<OvUI::Widgets::Layout::Spacing>();
+    --
+    --CreateWidget<OvUI::Widgets::Visual::Separator>();
+    --
+    --for (uint8_t i = 0; i < 4; ++i)
+    --	CreateWidget<OvUI::Widgets::Layout::Spacing>();
+    --auto& columns = CreateWidget<OvUI::Widgets::Layout::Columns<2>>();
+    --
+    --columns.widths = { 750, 500 };
+
+    self:ProjectList()
+
+
 end)
+
+function ProjectHubPanel:Header()
+    local openProjectButton = self:CreateWidget(Button, "Open Project")
+    local newProjectButton = self:CreateWidget(Button, "New Project")
+    local pathField = self:CreateWidget(InputText, "?");
+    local m_goButton = self:CreateWidget(Button, "GO")
+
+    -- void UpdateGoButton(const std::string& p_path)
+    --{
+    --bool validPath = p_path != "";
+    --m_goButton->idleBackgroundColor = validPath ? OvUI::Types::Color{ 0.f, 0.5f, 0.0f } : OvUI::Types::Color{ 0.1f, 0.1f, 0.1f };
+    --m_goButton->disabled = !validPath;
+    --}
+
+    local function UpdateGoButton(p_path)
+        validPath = p_path ~= ""
+        if validPath then
+            m_goButton.idleBackgroundColor = Vector3(0, 0.5, 0)
+        else
+            m_goButton.idleBackgroundColor = Vector3(0.1, 0.1, 0.1)
+        end
+        m_goButton.disabled = not validPath
+    end
+
+    UpdateGoButton("") -- init go button
+
+    openProjectButton.idleBackgroundColor = Vector3 { 0.7, 0.5, 0. };
+    newProjectButton.idleBackgroundColor = Vector3 { 0., 0.5, 0.0 };
+
+    openProjectButton.lineBreak = false;
+    newProjectButton.lineBreak = false;
+    pathField.lineBreak = false;
+
+
+    -- TODO
+    -- pathField.ContentChangedEvent += [this, &pathField](std::string p_content)
+    --{
+    --	pathField.content = OvTools::Utils::PathParser::MakeWindowsStyle(p_content);
+    --
+    --	if (pathField.content != "" && pathField.content.back() != '\\')
+    --		pathField.content += '\\';
+    --
+    --	UpdateGoButton(pathField.content);
+    --};
+
+    openProjectButton:AddOnClickHandler(function()
+        -- OvWindowing::Dialogs::OpenFileDialog dialog("Open project");
+        --dialog.AddFileType("Overload Project", "*.ovproject");
+        --dialog.Show();
+        --
+        --std::string ovProjectPath = dialog.GetSelectedFilePath();
+        --std::string rootFolderPath = OvTools::Utils::PathParser::GetContainingFolder(ovProjectPath);
+        --
+        --if (dialog.HasSucceeded())
+        --{
+        --	RegisterProject(rootFolderPath);
+        --	OpenProject(rootFolderPath);
+        --}
+    end)
+
+    newProjectButton:AddOnClickHandler(function()
+        --OvWindowing::Dialogs::SaveFileDialog dialog("New project location");
+        --dialog.DefineExtension("Overload Project", "..");
+        --dialog.Show();
+        --if (dialog.HasSucceeded())
+        --{
+        --	std::string result = dialog.GetSelectedFilePath();
+        --	pathField.content = std::string(result.data(), result.data() + result.size() - std::string("..").size()); // remove auto extension
+        --	pathField.content += "\\";
+        --	UpdateGoButton(pathField.content);
+        --}
+    end)
+
+    --m_goButton->ClickedEvent += [this, &pathField]
+    --{
+    --	CreateProject(pathField.content);
+    --	RegisterProject(pathField.content);
+    --	OpenProject(pathField.content);
+    --};
+
+    m_goButton:AddOnClickHandler(function()
+        -- TODO
+    end)
+end
+
+function ProjectHubPanel:ProjectList()
+
+end
 
 function ProjectHubPanel:Update()
     ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, 50, 50)
