3bb2d44e47c59e0bcb4b4800b9cb80a845f717ca
zoloypzuo <910181598@qq.com>
2021-08-23 21:34:55 +0800

    Update group.lua

--- Script/Lua/ui/layouts/group.lua
+++ Script/Lua/ui/layouts/group.lua
@@ -2,30 +2,14 @@
 -- created on 2021/8/22
 -- author @zoloypzuo
 local AWidget = require("ui.widget")
+local WidgetContainerMixin = require("ui.widget_container_mixin")
 
 local Group = Class(AWidget, function(self, parent)
     AWidget._ctor(self, parent)
-    self.widgets = {}
-end)
+end):include(WidgetContainerMixin)
 
 function Group:_UpdateImpl()
-    for _, widget in ipairs(self.widgets) do
-        widget:Update()
-    end
-end
-
-function Group:CreateWidget(type_, ...)
-    inst = type_(self, ...)
-    self.widgets[#self.widgets + 1] = inst
-    return inst
-end
-
-function Group:RemoveWidget(widget)
-    RemoveByValue(self.widgets, widget)
-end
-
-function Group:Clear()
-    self.widgets = {}
+    self:UpdateWidgets()
 end
 
 return Group
