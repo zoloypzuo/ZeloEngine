814f5694ccdd8e92014ab1a0779051ce77274596
左亦 <910181598@qq.com>
2021-08-24 15:05:11 +0800

    Create ui_consts.lua

---
+++ Script/Lua/ui/ui_consts.lua
@@ -0,0 +1,14 @@
+-- ui_consts
+-- created on 2021/8/24
+-- author @zoloypzuo
+EHorizontalAlignment = {
+    LEFT = 0,
+    CENTER = 1,
+    RIGHT = 2
+}
+
+EVerticalAlignment = {
+    TOP = 0,
+    MIDDLE = 1,
+    BOTTOM = 2
+}
