1383fb349be7de69e8d772755433a6cbc101d4db
左亦 <910181598@qq.com>
2021-08-25 15:21:44 +0800

    Create editor_actions.lua

---
+++ Script/Lua/editor/editor_actions.lua
@@ -0,0 +1,24 @@
+-- editor_actions
+-- created on 2021/8/25
+-- author @zoloypzuo
+local EditorActions = Class(function(self)
+
+end)
+
+function EditorActions:MoveToTarget()
+
+end
+
+function EditorActions:DelayAction()
+
+end
+
+function EditorActions:DestroyActor()
+
+end
+
+function EditorActions:UnselectActor()
+
+end
+
+TheEditorActions = EditorActions()
