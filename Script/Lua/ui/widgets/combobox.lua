c63f4b129b9ca43c31082ec5d00efbbe4e48a8e7
左亦 <910181598@qq.com>
2021-08-26 18:43:25 +0800

    Create combobox.lua

---
+++ Script/Lua/ui/widgets/combobox.lua
@@ -0,0 +1,51 @@
+-- combobox
+-- created on 2021/8/26
+-- author @zoloypzuo
+local AWidget = require("ui.widget")
+
+local ComboBox = Class(AWidget, function(self, parent, currentChoice)
+    AWidget._ctor(self, parent)
+    self.choices = {}  -- int => string
+    self.currentChoice = currentChoice or 0
+    self.value = value or false
+    self.label = label or ""
+
+    self.getter = nil
+    self.setter = nil
+
+    local processor = EventProcessor()
+    self.ValueChangedEvent = EventWrapper(processor, "ValueChangedEvent")
+end)
+
+function ComboBox:_UpdateImpl()
+    --if self.getter then
+    --    self.value = self.getter()
+    --end
+
+    if ImGui.BeginCombo(self.id, self.choices[self.currentChoice]) then
+        for key, value  in pairs(self.choices) do
+            local selected = key == self.currentChoice
+
+            if ImGui.Selectable(value, selected) then
+                if not selected then
+                    ImGui.SetItemDefaultFocus()
+                    self.currentChoice = key
+                    self.ValueChangedEvent:HandleEvent(value)
+                end
+            end
+        end
+    end
+
+    --if value ~= self.value then
+    --    self.value = value
+    --
+    --    if self.setter then
+    --        self.setter(value)
+    --    end
+    --
+    --end
+
+    self.value = value
+end
+
+return ComboBox
