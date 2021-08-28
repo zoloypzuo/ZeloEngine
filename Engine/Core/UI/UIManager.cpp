bec5ebf86c822a00ae9a4fb17b5bef2b06b0832d
zoloypzuo <910181598@qq.com>
2021-08-28 13:57:02 +0800

    Update UIManager.cpp

--- Engine/Core/UI/UIManager.cpp
+++ Engine/Core/UI/UIManager.cpp
@@ -139,6 +139,7 @@ void UIManager::update() {
     ImGui::End();
 
     // Script update logic here
+    // ImGui::ShowDemoWindow();
 }
 
 void UIManager::draw() {
@@ -224,36 +225,6 @@ void UIManager::ApplyStyle(UIManager::EStyle style) {
 //            imStyle->Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(1.00f, 0.98f, 0.95f, 0.73f);
 
             imStyle->Colors[ImGuiCol_Tab] = imStyle->Colors[ImGuiCol_TabUnfocused];
-
-            auto &colors = ImGui::GetStyle().Colors;
-            colors[ImGuiCol_WindowBg] = ImVec4{0.1f, 0.105f, 0.11f, 1.0f};
-
-            // Headers
-            colors[ImGuiCol_Header] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
-            colors[ImGuiCol_HeaderHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
-            colors[ImGuiCol_HeaderActive] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
-
-            // Buttons
-            colors[ImGuiCol_Button] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
-            colors[ImGuiCol_ButtonHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
-            colors[ImGuiCol_ButtonActive] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
-
-            // Frame BG
-            colors[ImGuiCol_FrameBg] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
-            colors[ImGuiCol_FrameBgHovered] = ImVec4{0.3f, 0.305f, 0.31f, 1.0f};
-            colors[ImGuiCol_FrameBgActive] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
-
-            // Tabs
-            colors[ImGuiCol_Tab] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
-            colors[ImGuiCol_TabHovered] = ImVec4{0.38f, 0.3805f, 0.381f, 1.0f};
-            colors[ImGuiCol_TabActive] = ImVec4{0.28f, 0.2805f, 0.281f, 1.0f};
-            colors[ImGuiCol_TabUnfocused] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
-            colors[ImGuiCol_TabUnfocusedActive] = ImVec4{0.2f, 0.205f, 0.21f, 1.0f};
-
-            // Title
-            colors[ImGuiCol_TitleBg] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
-            colors[ImGuiCol_TitleBgActive] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
-            colors[ImGuiCol_TitleBgCollapsed] = ImVec4{0.15f, 0.1505f, 0.151f, 1.0f};
             break;
         }
         case EStyle::ALTERNATIVE_DARK: {
@@ -361,7 +332,7 @@ std::string UIManager::SaveFileDialog() {
     return narrow;
 }
 
-void UIManager::MessageBox(int type, const std::string& title, const std::string& message) {
+void UIManager::MessageBox(int type, const std::string &title, const std::string &message) {
     auto flags = static_cast<uint32_t>(type);
     SDL_ShowSimpleMessageBox(flags, title.c_str(), message.c_str(), Window::getSingletonPtr()->getSDLWindow());
 }
