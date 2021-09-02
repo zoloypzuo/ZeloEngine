969f946ff8eeb73436ee417473084f2d2cfc1e0c
zoloypzuo <910181598@qq.com>
2021-09-02 23:17:44 +0800

    Update Zelo.h

--- Engine/Zelo.h
+++ Engine/Zelo.h
@@ -6,16 +6,6 @@
 #define ZELOENGINE_ZELO_H
 
 // INCLUDE HEADERS HERE
-#include "Core/RHI/Object/Light.h"
-#include "Core/RHI/Resource/Material.h"
-#include "Renderer/OpenGL/Resource/GLTexture.h"
-#include "Renderer/OpenGL/Drawable/MeshRenderer.h"
-#include "Renderer/OpenGL/GLRenderSystem.h"
-#include "Core/Parser/MeshLoader.h"
-#include "Core/ECS/Component/CFreeMove.h"
-#include "Core/ECS/Component/CFreeLook.h"
-#include "Core/Game/Game.h"
-#include "Core/Math/Mathf.h"
 #include "Engine.h"
 
 #endif //ZELOENGINE_ZELO_H
