c9d2d266accdd7d3deaa14a02c78f7336016ca50
zoloypzuo <910181598@qq.com>
2021-09-03 00:53:45 +0800

    GenFlagFromTable

--- Script/Lua/ui/ui_util.lua
+++ Script/Lua/ui/ui_util.lua
@@ -10,3 +10,71 @@ function ParseFlags(enumTable, flag)
     end
     return names
 end
+
+function GenFlagFromTable(imguiFlags, setting, default)
+    if default then
+        -- use default
+        for k, v in pairs(default) do
+            if setting[k] ~= nil then
+                setting[k] = v
+            end
+        end
+    end
+
+    local flag = 0
+    for k, v in pairs(setting) do
+        assert(imguiFlags[k], "flag not exists")
+        if v then
+            flag = bit.bor(flag, imguiFlags[k])
+        end
+    end
+    return flag
+end
+-- TODO DefaultPanelWindowSettings
+local DefaultPanelWindowSettings = {
+    closable = false;
+    resizable = true;
+    movable = true;
+    dockable = true;
+    scrollable = true;
+    hideBackground = false;
+    forceHorizontalScrollbar = false;
+    forceVerticalScrollbar = false;
+    allowHorizontalScrollbar = false;
+    bringToFrontOnFocus = true;
+    collapsable = false;
+    allowInputs = true;
+    titleBar = true;
+    autoSize = false;
+}
+
+local function GenFlagFromPaneSetting(panelSettings)
+    local resizable = panelSettings.resizable
+    local movable = panelSettings.movable
+    local dockable = panelSettings.dockable
+    local scrollable = panelSettings.scrollable
+    local hideBackground = panelSettings.hideBackground
+    local forceHorizontalScrollbar = panelSettings.forceHorizontalScrollbar
+    local forceVerticalScrollbar = panelSettings.forceVerticalScrollbar
+    local allowHorizontalScrollbar = panelSettings.allowHorizontalScrollbar
+    local bringToFrontOnFocus = panelSettings.bringToFrontOnFocus
+    local collapsable = panelSettings.collapsable
+    local allowInputs = panelSettings.allowInputs
+    local titleBar = panelSettings.titleBar
+
+    return GenFlagFromTable(ImGuiWindowFlags, {
+        NoResize = not resizable;
+        NoMove = not movable;
+        NoDocking = not dockable;
+        NoBackground = hideBackground;
+        AlwaysHorizontalScrollbar = forceHorizontalScrollbar;
+        AlwaysVerticalScrollbar = forceVerticalScrollbar;
+        HorizontalScrollbar = allowHorizontalScrollbar;
+        NoBringToFrontOnFocus = not bringToFrontOnFocus;
+        NoCollapse = not collapsable;
+        NoInputs = not allowInputs;
+        NoScrollWithMouse = not scrollable;
+        NoScrollbar = not scrollable;
+        NoTitleBar = not titleBar;
+    }, DefaultPanelWindowSettings)
+end
