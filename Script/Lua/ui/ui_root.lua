2c956455b82c1fae28b13d4a45af9b64302210b1
zoloypzuo <910181598@qq.com>
2021-09-04 11:54:48 +0800

    Update ui_root.lua

--- Script/Lua/ui/ui_root.lua
+++ Script/Lua/ui/ui_root.lua
@@ -1,17 +1,28 @@
 -- ui_root
 -- created on 2021/8/21
 -- author @zoloypzuo
+local TestDemoWindow = true
+
 UIRoot = Class(function(self)
     self.panels = {}
 end)
 
 function UIRoot:Update()
+    if TestDemoWindow then
+        local ShowDemoWindow = require("ui.demo.demo")
+        ShowDemoWindow(true)
+        return
+    end
     for _, panel in pairs(self.panels) do
         panel:Update()
     end
 end
 
 function UIRoot:LoadPanel(type_, ...)
+    if TestDemoWindow then
+        return
+    end
+
     assert(type(type_) == "table", "should load a panel")
     local panel = type_(...)
     self.panels[panel.id] = panel
