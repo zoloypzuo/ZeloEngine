5afe19cf5f6cdb79107b8931ae9a68bf104113bf
zoloypzuo <910181598@qq.com>
2021-09-11 12:35:15 +0800

    Update text.lua

--- Script/Lua/ui/widgets/text.lua
+++ Script/Lua/ui/widgets/text.lua
@@ -1,15 +1,6 @@
 -- button
 -- created on 2021/8/22
 -- author @zoloypzuo
-local AWidget = require("ui.widget")
+local WidgetSimple = require("ui.widget_simple")
 
-local Text = Class(AWidget, function(self, parent, content)
-    AWidget._ctor(self, parent)
-    self.content = content or ""
-end)
-
-function Text:_UpdateImpl()
-    ImGui.Text(self.content)
-end
-
-return Text
+return WidgetSimple("Text")
