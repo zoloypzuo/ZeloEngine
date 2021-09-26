d157ab941a39d35f2e773e504c431a68f785715b
zoloypzuo <910181598@qq.com>
2021-09-26 09:48:04 +0800

    Update widget_simple.lua

--- Script/Lua/ui/widget_simple.lua
+++ Script/Lua/ui/widget_simple.lua
@@ -5,12 +5,26 @@ local AWidget = require("ui.widget")
 
 local function WidgetSimple(name)
     -- simple stateless widget
+    --
+    -- Example:
+    -- local Text = Class(AWidget, function(self, parent, content)
+    --    AWidget._ctor(self, parent)
+    --    self.content = content or ""
+    -- end)
+    --
+    -- function Text:_UpdateImpl()
+    --    ImGui.Text(self.content)
+    -- end
+
+    -- =>
+    -- Text = WidgetSimple("Text")
     local cls = Class(AWidget, function(self, parent, ...)
         AWidget._ctor(self, parent)
         self.args = { ... }
     end)
 
     function cls:_UpdateImpl()
+        assert(ImGui[name])
         ImGui[name](table.unpack(self.args))
     end
 
@@ -18,16 +32,3 @@ local function WidgetSimple(name)
 end
 
 return WidgetSimple
-
--- example
---local Text = Class(AWidget, function(self, parent, content)
---    AWidget._ctor(self, parent)
---    self.content = content or ""
---end)
---
---function Text:_UpdateImpl()
---    ImGui.Text(self.content)
---end
-
--- =>
--- Text = WidgetSimple("Text")
