18b9d7ef2ab7a6d21318c0fef0f51858edee8205
zoloypzuo <910181598@qq.com>
2021-10-01 00:46:09 +0800

    style: prefab

--- Script/Lua/prefabs/monkey.lua
+++ Script/Lua/prefabs/monkey.lua
@@ -1,6 +1,10 @@
 -- plane
 -- created on 2021/8/9
 -- author @zoloypzuo
+local CreateEntity = CreateEntity
+local LoadResource = LoadResource
+local Prefab = Prefab
+
 local assets = {
     mesh = "monkey3.obj";
     material = "monkey.mat";
@@ -8,13 +12,11 @@ local assets = {
 
 local function fn()
     local inst = CreateEntity()
-    local trans = inst.entity:AddTransform()
+    inst.entity:AddTransform()
     inst:AddTag("monkey")
 
     local mesh_renderer = inst.entity:AddMeshRenderer()
-
     mesh_renderer.mesh = LoadResource(assets.mesh)
-
     mesh_renderer.material = LoadResource(assets.material)
 
     return inst
--- Script/Lua/prefabs/plane.lua
+++ Script/Lua/prefabs/plane.lua
@@ -1,6 +1,10 @@
 -- plane
 -- created on 2021/8/9
 -- author @zoloypzuo
+local CreateEntity = CreateEntity
+local LoadResource = LoadResource
+local Prefab = Prefab
+
 local assets = {
     mesh_gen = "plane.mesh_gen";
     material = "plane.mat";
@@ -8,13 +12,11 @@ local assets = {
 
 local function fn()
     local inst = CreateEntity()
-    local trans = inst.entity:AddTransform()
+    inst.entity:AddTransform()
     inst:AddTag("plane")
 
     local mesh_renderer = inst.entity:AddMeshRenderer()
-
     mesh_renderer.mesh = LoadResource(assets.mesh_gen)
-
     mesh_renderer.material = LoadResource(assets.material)
 
     return inst
