d7becb2d4b2a0d308011a5d4b603b43e23e14095
zoloypzuo <910181598@qq.com>
2021-10-14 15:24:22 +0800

    Create vcpkg_link.bat

---
+++ Tools/vcpkg_link.bat
@@ -0,0 +1,15 @@
+@echo off
+set CurrentDir=%cd%
+set ScriptDir=%~dp0
+set EngineDir=%ScriptDir%\..
+set Args=%*
+
+cd /d %EngineDir%
+@echo on
+cd ThirdParty
+
+rmdir /S /Q Vcpkg
+mklink /D /J Vcpkg D:\ins\vcpkg
+
+cd %CurrentDir%
+pause
