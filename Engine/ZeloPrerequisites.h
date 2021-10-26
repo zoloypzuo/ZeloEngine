74a1ded948d7b17fa56cbf57b55dfa42455de636
zoloypzuo <910181598@qq.com>
2021-10-26 10:13:38 +0800

    Update ZeloPrerequisites.h

--- Engine/ZeloPrerequisites.h
+++ Engine/ZeloPrerequisites.h
@@ -1,9 +1,7 @@
-//
-// Created by zoloypzuo on 2021/3/28.
-//
-
-#ifndef ZELOENGINE_ZELOPREREQUISITES_H
-#define ZELOENGINE_ZELOPREREQUISITES_H
+// ZeloPrerequisite.h
+// created on 2021/3/28.
+// author @zoloypzuo
+#pragma once
 
 #include "ZeloPlatform.h"
 
@@ -86,21 +84,7 @@ typedef int64_t GUID_t;
 #include "Core/Interface/IRuntimeModule.h"
 #include "Core/Interface/ISerializable.h"
 
-template<typename T>
-using Scope = std::unique_ptr<T>;
 
-template<typename T, typename ... Args>
-constexpr Scope<T> CreateScope(Args &&... args) {
-    return std::make_unique<T>(std::forward<Args>(args)...);
-}
-
-template<typename T>
-using Ref = std::shared_ptr<T>;
-
-template<typename T, typename ... Args>
-constexpr Ref<T> CreateRef(Args &&... args) {
-    return std::make_shared<T>(std::forward<Args>(args)...);
-}
 
 // forward declaration
 namespace Zelo { class Engine; }
@@ -120,4 +104,3 @@ class Component;
 
 class Behaviour;
 }
-#endif //ZELOENGINE_ZELOPREREQUISITES_H
