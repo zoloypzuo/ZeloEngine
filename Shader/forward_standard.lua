774cadf767498e4984c212f2482f2bafcb14945f
zoloypzuo <910181598@qq.com>
2021-11-07 20:07:20 +0800

    Create forward_standard.lua

---
+++ Shader/forward_standard.lua
@@ -0,0 +1,15 @@
+-- forward_standard
+-- created on 2021/11/7
+-- author @zoloypzuo
+local vertex_shader = [[
+
+]]
+
+local fragment_shader = [[
+
+]]
+
+return {
+    vertex_shader = vertex_shader,
+    fragment_shader = fragment_shader,
+}
