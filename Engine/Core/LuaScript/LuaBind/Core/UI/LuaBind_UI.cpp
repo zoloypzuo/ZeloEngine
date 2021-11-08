1a73f64f8d8be155298131d2d339b5ea3b88d2c6
zoloypzuo <910181598@qq.com>
2021-11-08 12:44:45 +0800

    fix new_enum

--- Engine/Core/LuaScript/LuaBind/Core/UI/LuaBind_UI.cpp
+++ Engine/Core/LuaScript/LuaBind/Core/UI/LuaBind_UI.cpp
@@ -8,14 +8,6 @@
 using namespace Zelo::Core::UI;
 
 void LuaBind_UI(sol::state &luaState) {
-// @formatter:off
-luaState.new_enum<UIManager::EStyle>("EStyle",{
-{"IM_CLASSIC_STYLE",UIManager::EStyle::IM_CLASSIC_STYLE},
-{"IM_DARK_STYLE",   UIManager::EStyle::IM_DARK_STYLE},
-{"IM_LIGHT_STYLE",  UIManager::EStyle::IM_LIGHT_STYLE},
-{"DUNE_DARK",       UIManager::EStyle::DUNE_DARK},
-{"ALTERNATIVE_DARK",UIManager::EStyle::ALTERNATIVE_DARK}
-});
 
 luaState.new_usertype<Font>("Font",
 sol::constructors<Font(const std::string&, float)>()
