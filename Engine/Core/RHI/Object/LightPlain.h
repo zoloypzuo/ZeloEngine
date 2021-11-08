65d3aa4510b3b0c3ca86a9472c756d99617f282f
zoloypzuo <910181598@qq.com>
2021-11-08 11:18:52 +0800

    light

---
+++ Engine/Core/RHI/Object/LightPlain.h
@@ -0,0 +1,42 @@
+// LightPlain.h
+// created on 2021/11/7
+// author @zoloypzuo
+#pragma once
+
+#include "ZeloPrerequisites.h"
+#include "ZeloGLPrerequisites.h"
+#include "Core/ECS/Entity.h"
+
+namespace Zelo::Core::RHI {
+enum class ELightType {
+    POINT, DIRECTIONAL, SPOT, AMBIENT_BOX, AMBIENT_SPHERE
+};
+
+class LightPlain : public Zelo::Core::ECS::Component,
+                   public std::enable_shared_from_this<LightPlain> {
+public:
+    explicit LightPlain(Zelo::Core::ECS::Entity &owner);
+
+    ~LightPlain() override;
+
+    std::string getType() override { return "Light"; }
+
+    void registerWithEngine() override;
+
+    void deregisterFromEngine() override;
+
+    // encode data into a matrix
+    glm::mat4 generateLightMatrix() const;
+
+public:
+    ELightType type = ELightType::POINT;
+    glm::vec3 color = {1.f, 1.f, 1.f};
+    float intensity = 1.f;
+    float constant = 0.0f;
+    float linear = 0.0f;
+    float quadratic = 1.0f;
+    float cutoff = 12.f;
+    float outerCutoff = 15.f;
+};
+}
+
