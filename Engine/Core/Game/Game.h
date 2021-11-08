c8377f334c11877fac93cdd112e9419960e6c5be
zoloypzuo <910181598@qq.com>
2021-11-08 11:26:48 +0800

    FastAccessComponents

--- Engine/Core/Game/Game.h
+++ Engine/Core/Game/Game.h
@@ -8,9 +8,19 @@
 #include "ZeloSingleton.h"
 #include "Core/RHI/Object/Camera.h"
 #include "Core/ECS/Entity.h"
+#include "Core/RHI/Object/LightPlain.h"
+#include "Renderer/OpenGL/Drawable/MeshRenderer.h"  // TODO extract to Core
 
 // TODO move namespace Zelo::Core::Game
+// TODO rename to Scene
 class Game : public Singleton<Game>, public IRuntimeModule {
+public:
+    struct FastAccessComponents {
+        std::vector<MeshRenderer *> meshRenderers;
+        std::vector<Camera *> cameras;
+        std::vector<Zelo::Core::RHI::LightPlain *> lights;
+    };
+
 public:
     Game();
 
@@ -28,6 +38,8 @@ public:
 public:
     std::shared_ptr<Zelo::Core::ECS::Entity> getRootNode();
 
+    const FastAccessComponents &getFastAccessComponents() const { return m_fastAccessComponents; }
+
 public:
     Zelo::GUID_t SpawnPrefab(const std::string &name);
 
@@ -38,6 +50,8 @@ public:
 private:
     std::shared_ptr<Zelo::Core::ECS::Entity> rootScene{};
     Zelo::GUID_t m_entityGuidCounter{};
+
+    FastAccessComponents m_fastAccessComponents;
 };
 
 #endif //ZELOENGINE_GAME_H
