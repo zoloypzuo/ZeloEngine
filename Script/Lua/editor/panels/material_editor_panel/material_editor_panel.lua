6d95b73d4256a7b429b67e3ec3566dccb36ad1fc
zoloypzuo <910181598@qq.com>
2021-11-11 21:39:34 +0800

    Create material_editor_panel.lua

---
+++ Script/Lua/editor/panels/material_editor_panel/material_editor_panel.lua
@@ -0,0 +1,35 @@
+-- project_setting_panel
+-- created on 2021/9/30
+-- author @zoloypzuo
+local PanelWindow = require("ui.panel_window")
+local Button = require("ui.widgets.button")
+local Separator = require("ui.widgets.separator")
+local Columns = require("ui.layouts.column")
+local GroupCollapsable = require("ui.layouts.group_collapsable")
+
+local MaterialEditorPanel = Class(PanelWindow, function(self, title, opened, panelSetting)
+    PanelWindow._ctor(self, title, opened, panelSetting)
+
+    local saveButton = self:CreateWidget(Button, "Apply")
+    saveButton.idleBackgroundColor = RGBA(0.0, 0.5, 0.0)
+    saveButton.lineBreak = false
+    saveButton.ClickedEvent:AddEventHandler(function()
+        print("Apply TODO")
+    end)
+
+    local resetButton = self:CreateWidget(Button, "Reset")
+    resetButton.idleBackgroundColor = RGBA(0.5, 0, 0)
+    resetButton.ClickedEvent:AddEventHandler(function()
+        print("Reset TODO")
+    end)
+
+    self:CreateWidget(Separator)
+
+    do
+        local rendererRoot = self:CreateWidget(GroupCollapsable, "Render")
+        local columns = rendererRoot:CreateWidget(Columns, 2)
+        columns.widths[1] = 125
+    end
+end)
+
+return MaterialEditorPanel
