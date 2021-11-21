b6314c3ed05438eff238e7bbc7d40674c331f7ba
zoloypzuo <910181598@qq.com>
2021-11-21 16:46:39 +0800

    extract template

--- Sandbox/GLSLBook/GLSLBookPlugins.h
+++ Sandbox/GLSLBook/GLSLBookPlugins.h
@@ -5,8 +5,14 @@
 
 #include "ZeloPrerequisites.h"
 #include "Core/Plugin/Plugin.h"
+#include "Core/RHI/RenderSystem.h"
 
-class EdgePipelinePlugin : public Plugin {
+#include "GLSLBook/ImageProcessing/EdgePipeline.h"
+#include "GLSLBook/ImageProcessing/BlurPipeline.h"
+#include "GLSLBook/ImageProcessing/BloomPipeline.h"
+
+template<class T>
+class PipelinePlugin : public Plugin {
 public:
     const std::string &getName() const override;;
 
@@ -19,28 +25,54 @@ public:
     void uninstall() override;
 };
 
-class BlurPipelinePlugin : public Plugin {
+template<class T>
+const std::string &PipelinePlugin<T>::getName() const {
+    static std::string s("");
+    return s;
+}
+
+template<class T>
+void PipelinePlugin<T>::install() {
+    auto renderPipeline = std::make_unique<T>();
+    renderPipeline->initialize();
+    Zelo::Core::RHI::RenderSystem::getSingletonPtr()->setRenderPipeline(std::move(renderPipeline));
+}
+
+template<class T>
+void PipelinePlugin<T>::initialise() {
+    // do nothing
+}
+
+template<class T>
+void PipelinePlugin<T>::shutdown() {
+    // do nothing
+}
+
+template<class T>
+void PipelinePlugin<T>::uninstall() {
+    Zelo::Core::RHI::RenderSystem::getSingletonPtr()->resetRenderPipeline();
+}
+
+class EdgePipelinePlugin : public PipelinePlugin<EdgePipeline> {
 public:
-    const std::string &getName() const override;;
-
-    void install() override;
-
-    void initialise() override;
-
-    void shutdown() override;
-
-    void uninstall() override;
+    const std::string &getName() const override {
+        static std::string s = "EdgePipelinePlugin";
+        return s;
+    }
 };
 
-class BloomPipelinePlugin : public Plugin {
+class BlurPipelinePlugin : public PipelinePlugin<BlurPipeline> {
 public:
-    const std::string &getName() const override;;
+    const std::string &getName() const override {
+        static std::string s = "BlurPipelinePlugin";
+        return s;
+    }
+};
 
-    void install() override;
-
-    void initialise() override;
-
-    void shutdown() override;
-
-    void uninstall() override;
+class BloomPipelinePlugin : public PipelinePlugin<BloomPipeline> {
+public:
+    const std::string &getName() const override {
+        static std::string s = "BloomPipelinePlugin";
+        return s;
+    }
 };
