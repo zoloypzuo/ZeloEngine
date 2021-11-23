c81a2215563b1fd5b861eaf9d04da4b442f359de
zoloypzuo <910181598@qq.com>
2021-11-23 14:52:05 +0800

    plugin

---
+++ Sandbox/Craft/CraftPlugin.cpp
@@ -0,0 +1,19 @@
+// CraftPlugin.cpp
+// created on 2021/11/23
+// author @zoloypzuo
+#include "ZeloPreCompiledHeader.h"
+#include "CraftPlugin.h"
+
+const std::string &CraftPlugin::getName() const {
+    static std::string s("CraftPlugin");
+    return s;
+}
+
+void CraftPlugin::install() {
+
+}
+
+void CraftPlugin::shutdown() {
+
+}
+
---
+++ Sandbox/Craft/CraftPlugin.h
@@ -0,0 +1,20 @@
+// CraftPlugin.h
+// created on 2021/11/23
+// author @zoloypzuo
+#pragma once
+
+#include "ZeloPrerequisites.h"
+#include "Core/Plugin/Plugin.h"
+
+class CraftPlugin : public Plugin {
+public:
+    const std::string &getName() const override;;
+
+    void install() override;
+
+    void initialise() override {}
+
+    void shutdown() override;
+
+    void uninstall() override {}
+};
