b587aebe55b1b44c7af5f99fcf2abf340868485e
zoloypzuo <910181598@qq.com>
2021-12-01 20:26:54 +0800

    Update cube.glsl

--- Shader/GRCookbook/cube.glsl
+++ Shader/GRCookbook/cube.glsl
@@ -35,17 +35,17 @@ vec3(-1.0, 1.0,-1.0)
 );
 
 const int indices[36] = int[36](
-// front
+/* front*/
 0, 1, 2, 2, 3, 0,
-// right
+/* right*/
 1, 5, 6, 6, 2, 1,
-// back
+/* back*/
 7, 6, 5, 5, 4, 7,
-// left
+/* left*/
 4, 0, 3, 3, 7, 4,
-// bottom
+/* bottom*/
 4, 5, 1, 1, 0, 4,
-// top
+/* top*/
 3, 2, 6, 6, 7, 3
 );
 
