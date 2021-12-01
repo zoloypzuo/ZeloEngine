f35cd5d6ca215a5f7ec8e2da6129796b9bb08daf
zoloypzuo <910181598@qq.com>
2021-12-01 16:29:59 +0800

    runAlphaTest

--- Shader/GRCookbook/mesh.glsl
+++ Shader/GRCookbook/mesh.glsl
@@ -93,24 +93,7 @@ layout (binding = 5) uniform samplerCube texEnvMap;
 layout (binding = 6) uniform samplerCube texEnvMapIrradiance;
 layout (binding = 7) uniform sampler2D texBRDF_LUT;
 
-void runAlphaTest(float alpha, float alphaThreshold)
-{
-    if (alphaThreshold > 0.0)
-    {
-        mat4 thresholdMatrix = mat4(
-        1.0  / 17.0,  9.0 / 17.0,  3.0 / 17.0, 11.0 / 17.0,
-        13.0 / 17.0,  5.0 / 17.0, 15.0 / 17.0,  7.0 / 17.0,
-        4.0  / 17.0, 12.0 / 17.0,  2.0 / 17.0, 10.0 / 17.0,
-        16.0 / 17.0,  8.0 / 17.0, 14.0 / 17.0,  6.0 / 17.0
-        );
-
-        alpha = clamp(alpha - 0.5 * thresholdMatrix[int(mod(gl_FragCoord.x, 4.0))][int(mod(gl_FragCoord.y, 4.0))], 0.0, 1.0);
-
-        if (alpha < alphaThreshold)
-        discard;
-    }
-}
-
+#include "alpha_test.fsh"
 #include "pbr_sp.fsh"
 
 void main()
