a5c3c357709548e6d8bd9a1a6965a92282db4894
zoloypzuo <910181598@qq.com>
2021-12-08 03:20:10 +0800

    -

---
+++ Tools/Setup/setup_appveyor.bat
@@ -0,0 +1,32 @@
+@echo off
+set CurrentDir=%cd%
+set ScriptDir=%~dp0
+set EngineDir=%ScriptDir%\..\..
+set Args=%*
+
+cd /d %EngineDir%
+@echo on
+
+if not exist Dep (
+    md Dep
+)
+
+cd %EngineDir%\Tools\Setup
+python check_prerequisite.py
+python bootstrap.py -b %EngineDir%\Dep --bootstrap-file bootstrap.json --skip vcpkg
+
+cd %EngineDir%\ThirdParty
+
+if not exist Vcpkg (
+    mklink /D /J Vcpkg c:\tools\vcpkg
+    pushd Vcpkg
+    git pull -q
+    popd
+)
+
+if not exist Vcpkg\vcpkg.exe (
+    call Vcpkg\bootstrap-vcpkg.bat -disableMetrics
+)
+
+
+cd %CurrentDir%
