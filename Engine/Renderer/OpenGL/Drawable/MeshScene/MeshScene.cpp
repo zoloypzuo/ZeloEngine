b7b1d464c113fa9e9bccc3bbd941f6ebdea51f74
zoloypzuo <910181598@qq.com>
2021-12-17 19:54:22 +0800

    commandbuffer

--- Engine/Renderer/OpenGL/Drawable/MeshScene/MeshScene.cpp
+++ Engine/Renderer/OpenGL/Drawable/MeshScene/MeshScene.cpp
@@ -21,12 +21,101 @@
 using namespace Zelo::Core::RHI;
 
 namespace Zelo::Renderer::OpenGL {
+class GLBuffer {
+public:
+    GLBuffer(uint32_t size, const void *data, uint32_t flags) {
+        glCreateBuffers(1, &m_RendererID);
+        glNamedBufferStorage(m_RendererID, size, data, flags);
+    }
+
+    ~GLBuffer() {
+        glDeleteBuffers(1, &m_RendererID);
+    }
+
+    uint32_t getHandle() const { return m_RendererID; }
+
+    virtual void bind() const { glBindBuffer(static_cast<GLenum>(getType()), m_RendererID); }
+
+    virtual void unbind() const { glBindBuffer(static_cast<GLenum>(getType()), 0); }
+
+    virtual GLBufferType getType() const = 0;
+
+protected:
+    uint32_t m_RendererID{};
+};
+
+class GLVertexBufferImmutable : public VertexBuffer, public GLBuffer {
+public:
+    GLVertexBufferImmutable(uint32_t size, const void *data, uint32_t flags) :
+            GLBuffer(size, data, flags) {}
+
+    ~GLVertexBufferImmutable() override = default;
+
+    void bind() const override { GLBuffer::bind(); }
+
+    void unbind() const override { GLBuffer::unbind(); }
+
+    GLBufferType getType() const override { return GLBufferType::ARRAY_BUFFER; }
+};
+
+class GLIndexBufferImmutable : public IndexBuffer, public GLBuffer {
+public:
+    GLIndexBufferImmutable(uint32_t size, const void *data, uint32_t flags) :
+            GLBuffer(size, data, flags) {}
+
+    ~GLIndexBufferImmutable() override = default;
+
+    void bind() const override { GLBuffer::bind(); }
+
+    void unbind() const override { GLBuffer::unbind(); }
+
+    GLBufferType getType() const override { return GLBufferType::ELEMENT_ARRAY_BUFFER; }
+
+    uint32_t getCount() const override { return 0; }
+};
+
 struct DrawElementsIndirectCommand {
     GLuint count_;
     GLuint instanceCount_;
     GLuint firstIndex_;
     GLuint baseVertex_;
     GLuint baseInstance_;
+};
+
+class GLIndirectCommandBuffer : public GLBuffer {
+public:
+    GLIndirectCommandBuffer(uint32_t size, const void *data, uint32_t flags, GLsizei numCommands) :
+            GLBuffer(size, data, flags) {
+
+        drawCommandBuffer.resize(size);
+
+        // store the number of draw commands in the very beginning of the buffer
+        memcpy(drawCommandBuffer.data(), &numCommands, sizeof(GLsizei));
+
+        auto *startOffset = drawCommandBuffer.data() + sizeof(GLsizei);
+        commandQueue = std::launder(reinterpret_cast<DrawElementsIndirectCommand *>(startOffset));
+    }
+
+    void bind() const override {
+        glBindBuffer(GL_DRAW_INDIRECT_BUFFER, m_RendererID);
+        glBindBuffer(GL_PARAMETER_BUFFER, m_RendererID);
+    }
+
+    ~GLIndirectCommandBuffer() = default;
+
+    GLBufferType getType() const override { return GLBufferType::DRAW_INDIRECT_BUFFER; }
+
+    void sendBlocks() {
+        glNamedBufferSubData(m_RendererID, 0, drawCommandBuffer.size(), drawCommandBuffer.data());
+    }
+
+    DrawElementsIndirectCommand *getCommandQueue() const { return commandQueue; }
+
+private:
+    // num of commands, followed by command queue
+    std::vector<uint8_t> drawCommandBuffer;
+    // start offset of command queue
+    DrawElementsIndirectCommand *commandQueue;
 };
 
 const static BufferLayout s_BufferLayout(
@@ -138,9 +227,11 @@ struct MeshScene::Impl {
     GLBufferImmutable bufferVertices_;
     GLBufferImmutable bufferMaterials_;
 
-    GLBufferImmutable bufferIndirect_;
+//    GLBufferImmutable bufferIndirect_;
 
     GLBufferImmutable bufferModelMatrices_;
+
+    std::unique_ptr<GLIndirectCommandBuffer> bufferIndirect_;
 
     explicit Impl() :
             numIndices_(g_SceneData->header_.indexDataSize / sizeof(uint32_t)),
@@ -148,8 +239,6 @@ struct MeshScene::Impl {
             bufferVertices_(g_SceneData->header_.vertexDataSize, g_SceneData->meshData_.vertexData_.data(), 0),
             bufferMaterials_(sizeof(MaterialDescription) * g_SceneData->materials_.size(),
                              g_SceneData->materials_.data(), 0),
-            bufferIndirect_(sizeof(DrawElementsIndirectCommand) * g_SceneData->shapes_.size() + sizeof(GLsizei),
-                            nullptr, GL_DYNAMIC_STORAGE_BIT),
             bufferModelMatrices_(sizeof(glm::mat4) * g_SceneData->shapes_.size(), nullptr, GL_DYNAMIC_STORAGE_BIT) {
 
         glCreateVertexArrays(1, &vao_);
@@ -168,32 +257,27 @@ struct MeshScene::Impl {
         glVertexArrayAttribFormat(vao_, 2, 3, GL_FLOAT, GL_TRUE, sizeof(vec3) + sizeof(vec2));
         glVertexArrayAttribBinding(vao_, 2, 0);
 
-        std::vector<uint8_t> drawCommands;
 
-        drawCommands.resize(sizeof(DrawElementsIndirectCommand) * g_SceneData->shapes_.size() + sizeof(GLsizei));
+        {
+            bufferIndirect_ = std::make_unique<GLIndirectCommandBuffer>(
+                    sizeof(DrawElementsIndirectCommand) * g_SceneData->shapes_.size() + sizeof(GLsizei),
+                    nullptr, GL_DYNAMIC_STORAGE_BIT, g_SceneData->shapes_.size());
+            // prepare indirect commands buffer
+            auto *cmd = bufferIndirect_->getCommandQueue();
+            for (size_t i = 0; i != g_SceneData->shapes_.size(); i++) {
+                const uint32_t meshIdx = g_SceneData->shapes_[i].meshIndex;
+                const uint32_t lod = g_SceneData->shapes_[i].LOD;
+                *cmd++ = {
+                        g_SceneData->meshData_.meshes_[meshIdx].getLODIndicesCount(lod),
+                        1,
+                        g_SceneData->shapes_[i].indexOffset,
+                        g_SceneData->shapes_[i].vertexOffset,
+                        g_SceneData->shapes_[i].materialIndex
+                };
+            }
 
-        // store the number of draw commands in the very beginning of the buffer
-        const GLsizei numCommands = (GLsizei) g_SceneData->shapes_.size();
-        memcpy(drawCommands.data(), &numCommands, sizeof(numCommands));
-
-        DrawElementsIndirectCommand *cmd = std::launder(
-                reinterpret_cast<DrawElementsIndirectCommand *>(drawCommands.data() + sizeof(GLsizei))
-        );
-
-        // prepare indirect commands buffer
-        for (size_t i = 0; i != g_SceneData->shapes_.size(); i++) {
-            const uint32_t meshIdx = g_SceneData->shapes_[i].meshIndex;
-            const uint32_t lod = g_SceneData->shapes_[i].LOD;
-            *cmd++ = {
-                    g_SceneData->meshData_.meshes_[meshIdx].getLODIndicesCount(lod),
-                    1,
-                    g_SceneData->shapes_[i].indexOffset,
-                    g_SceneData->shapes_[i].vertexOffset,
-                    g_SceneData->shapes_[i].materialIndex
-            };
+            bufferIndirect_->sendBlocks();
         }
-
-        glNamedBufferSubData(bufferIndirect_.getHandle(), 0, drawCommands.size(), drawCommands.data());
 
         std::vector<glm::mat4> matrices(g_SceneData->shapes_.size());
         size_t i = 0;
@@ -221,8 +305,7 @@ struct MeshScene::Impl {
         glBindVertexArray(vao_);
         glBindBufferBase(GL_SHADER_STORAGE_BUFFER, kBufferIndex_Materials, bufferMaterials_.getHandle());
         glBindBufferBase(GL_SHADER_STORAGE_BUFFER, kBufferIndex_ModelMatrices, bufferModelMatrices_.getHandle());
-        glBindBuffer(GL_DRAW_INDIRECT_BUFFER, bufferIndirect_.getHandle());
-        glBindBuffer(GL_PARAMETER_BUFFER, bufferIndirect_.getHandle());
+        bufferIndirect_->bind();
         glMultiDrawElementsIndirectCount(GL_TRIANGLES, GL_UNSIGNED_INT, (const void *) sizeof(GLsizei), 0,
                                          (GLsizei) g_SceneData->shapes_.size(), 0);
     }
