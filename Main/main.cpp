7b565135289c8879478c87095661da396270f9c7
zoloypzuo <910181598@qq.com>
2021-12-23 11:28:10 +0800

    backward

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,22 +1,12 @@
-#include "AutoSymInitialize.h"
-#include "AutoExceptionStacktraceRegister.h"
-#include "StackTracePrinter.h"
-
 #ifdef DetectMemoryLeak
 #include "Foundation/ZeloMemoryLeak.h"
 #endif
 
 #include "Zelo.h"
 
-using namespace ExceptionsStacktrace;
+#include "backward.hpp"
 
 int main() {
-    AutoSymInitialize autoSymInitialize;
-    AutoExceptionStacktraceRegister autoExceptionStacktraceRegister;
-    try {
-        Zelo::Engine().start();
-    } catch (...) {
-        messageBoxStacktrace();
-    }
+    Zelo::Engine().start();
     return 0;
 }
