99a1bf9217767c0686f4809ba867ba9495aac1db
zoloypzuo <910181598@qq.com>
2021-12-23 13:36:00 +0800

    fix windows

--- Engine/Zelo.h
+++ Engine/Zelo.h
@@ -5,16 +5,6 @@
 #ifndef ZELOENGINE_ZELO_H
 #define ZELOENGINE_ZELO_H
 
-#include "Foundation/ZeloPlatform.h"  // ZELO_PLATFORM_WINDOWS
-
-#ifdef ZELO_PLATFORM_WINDOWS
-
-// include windows before glad to fix:
-// warning C4005: 'APIENTRY': macro redefinition
-#include "Foundation/ZeloWindows.h"
-
-#endif
-
 // INCLUDE HEADERS HERE
 #include "Engine.h"
 
