7b4e344f3249e53706864b47a61df468c7f0b513
zoloypzuo <910181598@qq.com>
2021-12-24 21:40:55 +0800

    rm Craft

--- LuaBind/Sandbox/LuaBind_Sandbox.cpp
+++ LuaBind/Sandbox/LuaBind_Sandbox.cpp
@@ -3,7 +3,6 @@
 // author @zoloypzuo
 #include "Engine.h"
 
-#include "Craft//CraftPlugin.h"
 #include "GLSLBook/GLSLBookPlugins.h"
 #include "_Template//TemplatePlugins.h"
 
@@ -15,7 +14,6 @@ luaState.new_usertype<EdgePipelinePlugin>("EdgePipelinePlugin",sol::base_classes
 luaState.new_usertype<BlurPipelinePlugin>("BlurPipelinePlugin",sol::base_classes, sol::bases<Plugin>());
 luaState.new_usertype<BloomPipelinePlugin>("BloomPipelinePlugin",sol::base_classes, sol::bases<Plugin>());
 luaState.new_usertype<ShadowMapPipelinePlugin>("ShadowMapPipelinePlugin",sol::base_classes, sol::bases<Plugin>());
-luaState.new_usertype<CraftPlugin>("CraftPlugin",sol::base_classes, sol::bases<Plugin>());
 luaState.new_usertype<TemplatePlugin>("TemplatePlugin",sol::base_classes, sol::bases<Plugin>());
 // @formatter:on
 }
