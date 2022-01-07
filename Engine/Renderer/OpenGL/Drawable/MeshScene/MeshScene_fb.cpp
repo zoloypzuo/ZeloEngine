f176c22d405f82c9978526972458531195a74f36
zoloypzuo <910181598@qq.com>
2022-01-07 14:22:47 +0800

    Update MeshScene_fb.cpp

--- Engine/Renderer/OpenGL/Drawable/MeshScene/MeshScene_fb.cpp
+++ Engine/Renderer/OpenGL/Drawable/MeshScene/MeshScene_fb.cpp
@@ -10,13 +10,25 @@
 #include "Renderer/OpenGL/Drawable/MeshScene/Scene/SceneGraph.h"
 #include "Renderer/OpenGL/Drawable/MeshScene/VtxData/MeshData.h"
 
+namespace fb = flatbuffers;
+
+using namespace Zelo::Renderer::OpenGL;
+
 namespace Zelo::Renderer::OpenGL {
 void loadScene(const char *fileName, SceneGraph &scene) {
 
 }
 
 void saveScene(const char *fileName, const SceneGraph &scene) {
+    fb::SceneDataT sceneDataT{
+            .meshData = std::make_unique<fb::MeshDataT>(),
+            .sceneGraph = std::make_unique<fb::SceneGraphT>(),
+            .material = std::make_unique<fb::MaterialT>()
+    };
 
+    flatbuffers::FlatBufferBuilder fbb;
+    fbb.Finish(fb::SceneData::Pack(fbb, &sceneDataT));
+    flatbuffers::SaveFile(fileName, reinterpret_cast<char *>(fbb.GetBufferPointer()), fbb.GetSize(), true);
 }
 
 void saveMaterials(const char *fileName, const std::vector<MaterialDescription> &materials,
