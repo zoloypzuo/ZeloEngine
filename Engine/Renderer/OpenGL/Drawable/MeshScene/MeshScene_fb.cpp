b4d1d69980d951fa721f52f37f56b746f2b0f863
zoloypzuo <910181598@qq.com>
2022-01-07 14:16:40 +0800

    Update MeshScene_fb.cpp

--- Engine/Renderer/OpenGL/Drawable/MeshScene/MeshScene_fb.cpp
+++ Engine/Renderer/OpenGL/Drawable/MeshScene/MeshScene_fb.cpp
@@ -6,3 +6,33 @@
 
 #include <flatbuffers/util.h>
 
+#include "Renderer/OpenGL/Drawable/MeshScene/Material/Material.h"
+#include "Renderer/OpenGL/Drawable/MeshScene/Scene/SceneGraph.h"
+#include "Renderer/OpenGL/Drawable/MeshScene/VtxData/MeshData.h"
+
+namespace Zelo::Renderer::OpenGL {
+void loadScene(const char *fileName, SceneGraph &scene) {
+
+}
+
+void saveScene(const char *fileName, const SceneGraph &scene) {
+
+}
+
+void saveMaterials(const char *fileName, const std::vector<MaterialDescription> &materials,
+                   const std::vector<std::string> &files) {
+
+}
+
+void loadMaterials(const char *fileName, std::vector<MaterialDescription> &materials, std::vector<std::string> &files) {
+
+}
+}
+
+MeshFileHeader loadMeshData(const char *meshFile, MeshData &out) {
+    return {};
+}
+
+void saveMeshData(const char *fileName, const MeshData &m) {
+
+}
