ed89f8b21de074d6316c8bd580ad7b42fce3f85a
zoloypzuo <910181598@qq.com>
2022-01-07 16:35:24 +0800

    Update MeshScene_fb.cpp

--- Engine/Renderer/OpenGL/Drawable/MeshScene/MeshScene_fb.cpp
+++ Engine/Renderer/OpenGL/Drawable/MeshScene/MeshScene_fb.cpp
@@ -1,7 +1,7 @@
 // SceneImporter.cpp
 // created on 2022/1/7
 // author @zoloypzuo
-
+#include "ZeloPreCompiledHeader.h"
 #include "SceneData_generated.h"
 
 #include <flatbuffers/util.h>
@@ -15,29 +15,91 @@ namespace fb = flatbuffers;
 using namespace Zelo::Renderer::OpenGL;
 
 namespace Zelo::Renderer::OpenGL {
-void loadScene(const char *fileName, SceneGraph &scene) {
+template<typename T, typename U, typename Fn>
+std::vector<U> map(const std::vector<T> &inVec, Fn functor) {
+    std::vector<U> ret;
+    ret.reserve(inVec.size());
+    std::transform(inVec.begin(), inVec.end(), ret.begin(), functor);
+    return ret;
+}
 
+fb::Matrix4x4 GlmToFbMat4(glm::mat4 inMat) {
+    float data[16]{};
+    std::memcpy(glm::value_ptr(inMat), data, sizeof(data));
+    return fb::Matrix4x4(data);
+}
+
+glm::mat4 FbToGlmMat4(const fb::Matrix4x4 &from) {
+    return glm::make_mat4(from.data()->data());
+}
+
+fb::Vector3 GlmToFbVec3(glm::vec3 inVec) {
+    return fb::Vector3(inVec.x, inVec.y, inVec.z);
+}
+
+glm::vec3 FbToGlmVec3(fb::Vector3 inVec) {
+    return glm::vec3(inVec.x(), inVec.y(), inVec.z());
+}
+
+fb::Vector4 GpuToFbVec4(gpuvec4 inVec) {
+    return fb::Vector4(inVec.x, inVec.y, inVec.z, inVec.w);
+}
+
+fb::MaterialDescription ToFbMaterialDescription(const MaterialDescription &D) {
+    return {
+            GpuToFbVec4(D.emissiveColor_),
+            GpuToFbVec4(D.albedoColor_),
+            GpuToFbVec4(D.roughness_),
+            D.transparencyFactor_,
+            D.alphaTest_,
+            D.metallicFactor_,
+            D.flags_,
+            D.ambientOcclusionMap_,
+            D.emissiveMap_,
+            D.albedoMap_,
+            D.metallicRoughnessMap_,
+            D.normalMap_,
+            D.opacityMap_
+    };
+}
+
+void loadScene(const char *fileName, SceneGraph &scene) {
 }
 
 void saveScene(const char *fileName, const SceneGraph &scene) {
-    fb::SceneDataT sceneDataT{
-            .meshData = std::make_unique<fb::MeshDataT>(),
-            .sceneGraph = std::make_unique<fb::SceneGraphT>(),
-            .material = std::make_unique<fb::MaterialT>()
-    };
+    fb::FlatBufferBuilder builder;
+    fb::SceneGraphBuilder sceneGraphBuilder(builder);
 
-    flatbuffers::FlatBufferBuilder fbb;
-    fbb.Finish(fb::SceneData::Pack(fbb, &sceneDataT));
-    flatbuffers::SaveFile(fileName, reinterpret_cast<char *>(fbb.GetBufferPointer()), fbb.GetSize(), true);
+    sceneGraphBuilder.add_localTransform_(builder.CreateVectorOfStructs(
+            map<glm::mat4, fb::Matrix4x4>(scene.localTransform_, GlmToFbMat4)));
+//    sceneGraphBuilder.add_globalTransform_(builder.CreateVector())
+//    sceneGraphBuilder.add_hierarchy_()
+
+    ZELO_ASSERT(fb::SaveFile(fileName, (const char *) builder.GetBufferPointer(), builder.GetSize(), true));
+}
+
+void loadMaterials(const char *fileName, std::vector<MaterialDescription> &materials, std::vector<std::string> &files) {
+    std::string buf;
+    fb::LoadFile(fileName, true, &buf);
+    auto *material = fb::GetRoot<fb::Material>(&buf);
 }
 
 void saveMaterials(const char *fileName, const std::vector<MaterialDescription> &materials,
                    const std::vector<std::string> &files) {
+    fb::FlatBufferBuilder builder;
 
-}
+    auto fbMaterials = map<MaterialDescription, fb::MaterialDescription>(materials, ToFbMaterialDescription);
+    auto functor = [&builder](const std::string &s) -> fb::Offset<fb::String> { return builder.CreateString(s); };
+    auto fbFiles = map<std::string, fb::Offset<fb::String>>(files, functor);
+    auto material = fb::CreateMaterialDirect(
+            builder,
+            &fbMaterials,
+            &fbFiles
+    );
 
-void loadMaterials(const char *fileName, std::vector<MaterialDescription> &materials, std::vector<std::string> &files) {
+    builder.Finish(material);
 
+    ZELO_ASSERT(fb::SaveFile(fileName, (const char *) builder.GetBufferPointer(), builder.GetSize(), true));
 }
 
 MeshFileHeader loadMeshData(const char *meshFile, MeshData &out) {
@@ -45,6 +107,10 @@ MeshFileHeader loadMeshData(const char *meshFile, MeshData &out) {
 }
 
 void saveMeshData(const char *fileName, const MeshData &m) {
+    fb::FlatBufferBuilder builder;
 
+
+
+    ZELO_ASSERT(fb::SaveFile(fileName, (const char *) builder.GetBufferPointer(), builder.GetSize(), true));
 }
 }
