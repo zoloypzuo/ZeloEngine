05f3383b82f792c3800808dfc607934543d53d51
zoloypzuo <910181598@qq.com>
2022-01-08 11:51:50 +0800

    Update bistro.scene.lua

--- ResourceDB/Entities/Models/bistro.scene.lua
+++ ResourceDB/Entities/Models/bistro.scene.lua
@@ -3,11 +3,8 @@
 -- author @zoloypzuo
 return {
     type = "SCENE";
-    --scene_file = "meshes/test.scene";
-    --mesh_file = "meshes/test.meshes";
-    --material_file = "meshes/test.materials";
-    scene_file = "meshes/bistro_all.meshes";
-    mesh_file = "meshes/bistro_all.scene";
-    material_file = "meshes/bistro_all.materials";
+    scene_file = "meshes/bistro.meshes";
+    mesh_file = "meshes/bistro.scene";
+    material_file = "meshes/bistro.materials";
     dummy_texture_file = "const1.bmp";
 }
