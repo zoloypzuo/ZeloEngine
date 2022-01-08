797a03e3ce69774d8e373d5ba69ffaffd23f3c7b
zoloypzuo <910181598@qq.com>
2022-01-08 17:33:48 +0800

    Update plugins.lua

--- Script/Lua/plugins.lua
+++ Script/Lua/plugins.lua
@@ -2,19 +2,9 @@
 -- created on 2021/12/16
 -- author @zoloypzuo
 -- plugin
---postEffectPlugin = EdgePipelinePlugin.new()
---postEffectPlugin = BlurPipelinePlugin.new()
---postEffectPlugin = BloomPipelinePlugin.new()
---postEffectPlugin = ShadowMapPipelinePlugin.new()
-
 local plugins = {
     --"TemplatePlugin",
-    --"CraftPlugin",
-    --"Ch5MeshRendererPlugin",
-    --"Ch6PBRPlugin",
-    --"Ch7LargeScenePlugin",
-    --"Ch10FinalPlugin",
-    --"ImGuiManager",
+    "ImGuiManager",
 }
 
 global("PluginInstances")
