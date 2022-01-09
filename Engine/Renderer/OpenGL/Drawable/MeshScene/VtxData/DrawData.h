27515eb92148a848fc4305bcb883a886f63f83ed
zoloypzuo <910181598@qq.com>
2022-01-09 15:16:52 +0800

    Update DrawData.h

--- Engine/Renderer/OpenGL/Drawable/MeshScene/VtxData/DrawData.h
+++ Engine/Renderer/OpenGL/Drawable/MeshScene/VtxData/DrawData.h
@@ -1,11 +1,18 @@
 #pragma once
 
+/// contains information for rendering a mesh instance with a specific material.
 struct DrawData {
+    /// offsets into GPU buffers
     uint32_t meshIndex{};
+    /// offsets into GPU buffers
     uint32_t materialIndex{};
+    /// the relative offset to the vertex data
     uint32_t LOD{};
+    /// byte offsets into the mesh index and geometry buffers
     uint32_t indexOffset{};
+    /// byte offsets into the mesh index and geometry buffers
     uint32_t vertexOffset{};
+    /// the index of the global object-to-world-space transformation that's calculated by scene graph routines
     uint32_t transformIndex{};
 
     DrawData(uint32_t meshIndex_,
