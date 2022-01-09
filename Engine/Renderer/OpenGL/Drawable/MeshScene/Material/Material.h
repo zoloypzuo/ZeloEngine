35b7aa3bbcafe417145cc238ef685bfcd82eb058
左亦 <910181598@qq.com>
2022-01-09 15:17:31 +0800

    Cr material (#366)
    * Update Material.h
    
    * Update DrawData.h
    
    * Update SceneImporter.cpp

--- Engine/Renderer/OpenGL/Drawable/MeshScene/Material/Material.h
+++ Engine/Renderer/OpenGL/Drawable/MeshScene/Material/Material.h
@@ -15,23 +15,33 @@ enum MaterialFlags {
 
 constexpr const uint64_t INVALID_TEXTURE = 0xFFFFFFFF;
 
+/// Our material system is compatible with the glTF2 material format and easily extensible for
+/// incorporating many existing glTF2 extensions.
 struct PACKED_STRUCT MaterialDescription final {
+    /// emissive color
     gpuvec4 emissiveColor_ = {0.0f, 0.0f, 0.0f, 0.0f};
+    /// ambient color
     gpuvec4 albedoColor_ = {1.0f, 1.0f, 1.0f, 1.0f};
-    // UV anisotropic roughness (isotropic lighting models use only the first value). ZW values are ignored
+    /// surface's roughness, can be used to represent anisotropic roughness.
+    /// UV anisotropic roughness (isotropic lighting models use only the first value). ZW values are ignored
     gpuvec4 roughness_ = {1.0f, 1.0f, 0.0f, 0.0f};
+    /// used to render with alpha-blended materials
     float transparencyFactor_ = 1.0f;
+    /// alpha test threshold, which is used for the simple punch-through transparency rendering
     float alphaTest_ = 0.0f;
+    /// the metallic factor for our PBR rendering
     float metallicFactor_ = 0.0f;
     uint32_t flags_ = sMaterialFlags_CastShadow | sMaterialFlags_ReceiveShadow;
-    // maps
+#pragma region maps
     uint64_t ambientOcclusionMap_ = INVALID_TEXTURE;
     uint64_t emissiveMap_ = INVALID_TEXTURE;
     uint64_t albedoMap_ = INVALID_TEXTURE;
     /// Occlusion (R), Roughness (G), Metallic (B) https://github.com/KhronosGroup/glTF/issues/857
     uint64_t metallicRoughnessMap_ = INVALID_TEXTURE;
     uint64_t normalMap_ = INVALID_TEXTURE;
+    /// only used in SceneImporter
     uint64_t opacityMap_ = INVALID_TEXTURE;
+#pragma endregion maps
 };
 
 static_assert(sizeof(MaterialDescription) % 16 == 0, "MaterialDescription should be padded to 16 bytes");
