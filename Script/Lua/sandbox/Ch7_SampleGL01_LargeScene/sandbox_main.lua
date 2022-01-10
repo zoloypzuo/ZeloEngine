5ae5b449a3400a4b1a41ab6a5ce00057d22acc6e
zoloypzuo <910181598@qq.com>
2022-01-10 17:49:25 +0800

    Create sandbox_main.lua

---
+++ Script/Lua/sandbox/Ch7_SampleGL01_LargeScene/sandbox_main.lua
@@ -0,0 +1,16 @@
+local _M = {}
+
+function _M.Sandbox_Initialize()
+    LoadAvatar()
+    SpawnPrefab("bistro")
+end
+
+function _M.Sandbox_Update()
+
+end
+
+function _M.Sandbox_Finalize()
+
+end
+
+return _M
