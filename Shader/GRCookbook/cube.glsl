5b938cba745cb8f3f93b4ba767ea94fd41c75ad6
zoloypzuo <910181598@qq.com>
2022-01-10 11:25:43 +0800

    Update cube.glsl

--- Shader/GRCookbook/cube.glsl
+++ Shader/GRCookbook/cube.glsl
@@ -8,45 +8,33 @@ layout(std140, binding = 0) uniform PerFrameData
     uniform vec4 cameraPos;
 };
 
-struct Vertex
-{
-    float p[3];
-    float n[3];
-    float tc[2];
-};
-
-layout(std430, binding = 1) restrict readonly buffer Vertices
-{
-    Vertex in_Vertices[];
-};
-
 layout (location=0) out vec3 dir;
 
 const vec3 pos[8] = vec3[8](
-vec3(-1.0,-1.0, 1.0),
-vec3( 1.0,-1.0, 1.0),
-vec3( 1.0, 1.0, 1.0),
-vec3(-1.0, 1.0, 1.0),
+    vec3(-1.0,-1.0, 1.0),
+    vec3( 1.0,-1.0, 1.0),
+    vec3( 1.0, 1.0, 1.0),
+    vec3(-1.0, 1.0, 1.0),
 
-vec3(-1.0,-1.0,-1.0),
-vec3( 1.0,-1.0,-1.0),
-vec3( 1.0, 1.0,-1.0),
-vec3(-1.0, 1.0,-1.0)
+    vec3(-1.0,-1.0,-1.0),
+    vec3( 1.0,-1.0,-1.0),
+    vec3( 1.0, 1.0,-1.0),
+    vec3(-1.0, 1.0,-1.0)
 );
 
 const int indices[36] = int[36](
-/* front*/
-0, 1, 2, 2, 3, 0,
-/* right*/
-1, 5, 6, 6, 2, 1,
-/* back*/
-7, 6, 5, 5, 4, 7,
-/* left*/
-4, 0, 3, 3, 7, 4,
-/* bottom*/
-4, 5, 1, 1, 0, 4,
-/* top*/
-3, 2, 6, 6, 7, 3
+    /* front*/
+    0, 1, 2, 2, 3, 0,
+    /* right*/
+    1, 5, 6, 6, 2, 1,
+    /* back*/
+    7, 6, 5, 5, 4, 7,
+    /* left*/
+    4, 0, 3, 3, 7, 4,
+    /* bottom*/
+    4, 5, 1, 1, 0, 4,
+    /* top*/
+    3, 2, 6, 6, 7, 3
 );
 
 void main()
