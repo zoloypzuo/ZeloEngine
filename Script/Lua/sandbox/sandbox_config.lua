8843dfd028e76984b8752a6b2e768217bb1c443c
zoloypzuo <910181598@qq.com>
2022-01-10 23:22:44 +0800

    ren Ch5_SampleGL03_MeshRenderer

--- Script/Lua/sandbox/sandbox_config.lua
+++ Script/Lua/sandbox/sandbox_config.lua
@@ -10,7 +10,7 @@ end)
 return {
     SandboxConfig("Default");
     SandboxConfig("Ch3_SampleGL03_CubeMap");
-    SandboxConfig("GL03_MeshRenderer");
+    SandboxConfig("Ch5_SampleGL03_MeshRenderer");
     SandboxConfig("Ch7_SampleGL01_LargeScene");
     SandboxConfig("Ch10_SampleGL05_Final");
     SandboxConfig("GLSLBook.Bloom");
