82036dcdcb2d4a8b958d336e52e576939d769f1f
zoloypzuo <910181598@qq.com>
2022-01-10 11:26:32 +0800

    G

---
+++ Engine/G.cpp
@@ -0,0 +1,11 @@
+// G.cpp
+// created on 2022/1/10
+// author @zoloypzuo
+#include "ZeloPreCompiledHeader.h"
+#include "G.h"
+
+using namespace Zelo;
+using namespace Zelo::Core::EventSystem;
+
+Event<> G::s_DestroyedEvent;
+Event<> G::s_CreatedEvent;
---
+++ Engine/G.h
@@ -0,0 +1,15 @@
+// G.h
+// created on 2022/1/10
+// author @zoloypzuo
+#pragma once
+
+#include "ZeloPrerequisites.h"
+#include "Foundation/ZeloEvent.h"
+
+namespace Zelo {
+class G {
+public:
+    static Core::EventSystem::Event<> s_DestroyedEvent;
+    static Core::EventSystem::Event<> s_CreatedEvent;
+};
+}
