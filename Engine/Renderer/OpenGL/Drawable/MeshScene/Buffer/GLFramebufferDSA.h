8707c9ba6361168a7c1dcbf709216fc98111489b
zoloypzuo <910181598@qq.com>
2022-01-11 14:26:24 +0800

    GLFramebufferDSA

--- Engine/Renderer/OpenGL/Drawable/MeshScene/Buffer/GLFramebufferDSA.h
+++ Engine/Renderer/OpenGL/Drawable/MeshScene/Buffer/GLFramebufferDSA.h
@@ -1,12 +1,21 @@
 ﻿#pragma once
 
+#include "ZeloPrerequisites.h"
 #include "ZeloGLPrerequisites.h"
-#include <memory>
 #include "Renderer/OpenGL/Drawable/MeshScene/Texture/GLTexture.h"
 
 namespace Zelo::Renderer::OpenGL {
 class GLFramebufferDSA {
 public:
+    /// The constructor takes dimensions of the framebuffer and texture formats
+    /// for color and depth buffers. Whenever a texture format is set to 0, no
+    /// corresponding buffer is created. This is handy when we need color-only
+    /// framebuffers for fullscreen rendering or depth-only framebuffers for
+    /// shadow map rendering
+    /// \param width
+    /// \param height
+    /// \param formatColor glTextureStorage2D, set 0 to disable
+    /// \param formatDepth glTextureStorage2D, set 0 to disable
     GLFramebufferDSA(int width, int height, GLenum formatColor, GLenum formatDepth);
 
     ~GLFramebufferDSA();
@@ -17,11 +26,11 @@ public:
 
     GLuint getHandle() const { return handle_; }
 
-    const GLTexture &getTextureColor() const { return *texColor_.get(); }
+    const GLTexture &getTextureColor() const { return *texColor_; }
 
-    const GLTexture &getTextureDepth() const { return *texDepth_.get(); }
+    const GLTexture &getTextureDepth() const { return *texDepth_; }
 
-    void bind();
+    void bind() const;
 
     void unbind();
 
